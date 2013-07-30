5f6a92db1e86a9bd89f2bd2247e8882dabc1b3d8
Allan Jardine <allan.jardine@sprymedia.co.uk>
2013-07-30 14:57:54 +0100

    Update: If there is no sorting methods for the column type, use string
    - In the case of filtering, if there is no filtering extension for the
      column type, then the basic string based filter formatting is
      performed (i.e. string is always the fallback - there can't be an
      error because a function isn't there).
    
    - This commit matches that behaviour in sorting. If you define a column
      type as something for which there is no column sorting function, then
      the string type will be used rather than resulting in a javascript
      error.

--- js/core/core.sort.js
+++ js/core/core.sort.js
@@ -133,7 +133,7 @@ function _fnSort ( oSettings )
 			// methods.
 			displayMaster.sort( function ( a, b ) {
 				var
-					x, y, k, l, test, sort,
+					x, y, k, l, test, sort, fn,
 					len=aSort.length,
 					dataA = aoData[a]._aSortData,
 					dataB = aoData[b]._aSortData;
@@ -144,7 +144,8 @@ function _fnSort ( oSettings )
 					x = dataA[ sort.col ];
 					y = dataB[ sort.col ];
 
-					test = oExtSort[ sort.type+"-"+sort.dir ]( x, y );
+					fn = oExtSort[ sort.type+"-"+sort.dir ] || oExtSort[ "string-"+sort.dir ];
+					test = fn( x, y );
 					if ( test !== 0 ) {
 						return test;
 					}
