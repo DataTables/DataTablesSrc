e0f36e7c1ee9bd20ee1c5e6406836dba90136507
Allan Jardine <allan.jardine@sprymedia.co.uk>
2013-11-13 16:19:00 +0000

    New: Api methods - to$, toJQuery and join
    - to$ - Convert the result set to a jQuery object. Compliment to the
      toArray method. This is mainly useful when working with the node() and
      nodes() methods.
    
    - toJQuery - Alias of to$
    
    - join - Array join method

--- js/api/api.base.js
+++ js/api/api.base.js
@@ -269,6 +269,9 @@ _Api.prototype = /** @lends DataTables.Api */{
 	},
 
 
+	join:    __arrayProto.join,
+
+
 	indexOf: __arrayProto.indexOf || function (obj, start)
 	{
 		for ( var i=(start || 0), ien=this.length ; i<ien ; i++ ) {
@@ -463,6 +466,18 @@ _Api.prototype = /** @lends DataTables.Api */{
 	toArray: function ()
 	{
 		return __arrayProto.slice.call( this );
+	},
+
+
+	to$: function ()
+	{
+		return $( this );
+	},
+
+
+	toJQuery: function ()
+	{
+		return $( this );
 	},
 
 
