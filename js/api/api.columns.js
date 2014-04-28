DataTables/DataTablesSrc

commit d9f416232bb33d53cc9a6f006416bf19c385ebe4

Fix: Selector couldn't be given as an API instance with indexes
- It is useful to be able to filter row indexes by a condition and then
  operate on the resulting array of indexes, but before the selector
  would only itterate over real arrays. So if you passed is an API
  instance with the result set being the indexes to delete, it wouldn't
  work!

- Fix is to loop over anything which has a `length` property

- Thread 20572

js/api/api.selectors.js
@@ -7,7 +7,7 @@ var _selector_run = function ( selector, select )
 		out = [], res,
 		a, i, ien, j, jen;
 
-	if ( ! $.isArray( selector ) ) {
+	if ( ! selector || selector.length === undefined ) {
 		selector = [ selector ];
 	}
 

commit 8544842d7637d7da707b5674f4113eec345081f3

Fix thread 20096 - Visible column index selector
- The column index selector was broken by a previous commit (d9f416232).
- Fix also an error in the column selector whereby a result was being
  tested against itself

js/api/api.columns.js
@@ -38,7 +38,7 @@ var __column_selector = function ( settings, selector, opts )
 			];
 		}
 		else {
-			var match = typeof match === 'string' ?
+			var match = typeof s === 'string' ?
 				s.match( __re_column_selector ) :
 				'';
 

js/api/api.selectors.js
@@ -7,7 +7,9 @@ var _selector_run = function ( selector, select )
 		out = [], res,
 		a, i, ien, j, jen;
 
-	if ( ! selector || selector.length === undefined ) {
+	// Can't just check for isArray here, as an API or jQuery instance might be
+	// given with their array like look
+	if ( ! selector || typeof selector === 'string' || selector.length === undefined ) {
 		selector = [ selector ];
 	}
 