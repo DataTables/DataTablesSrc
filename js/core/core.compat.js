9339a81d180d079fb9729a0f2f52e0d176723d5f
Allan Jardine <allan.jardine@sprymedia.co.uk>
2014-10-09 16:20:27 +0100

    Dev: Remove debug line

--- js/core/core.compat.js
+++ js/core/core.compat.js
@@ -23,7 +23,6 @@ function _fnHungarianMap ( o )
 			newKey = key.replace( match[0], match[2].toLowerCase() );
 			map[ newKey ] = key;
 
-			//console.log( key, match );
 			if ( match[1] === 'o' )
 			{
 				_fnHungarianMap( o[key] );
