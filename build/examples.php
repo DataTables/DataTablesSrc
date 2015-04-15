986ef7a8167338ed5ad306d49fed27b46d68cb91
Allan Jardine <allan.jardine@sprymedia.co.uk>
2015-04-15 17:00:20 +0100

    Dev: Add Select and Buttons libraries to the list of aliased libraries for examples
    Select and Buttons are still _very_ much in development and not for
    general use yet, but their examples need this in order to build

--- build/examples.php
+++ build/examples.php
@@ -102,6 +102,8 @@ if ( isset( $options['cdn'] ) ) {
 	DT_Example::$lookup_libraries['js' ]['datatables']      = '//cdn.datatables.net/'.$versions['DataTables']['release']['version'].'/js/jquery.dataTables.min.js';
 	DT_Example::$lookup_libraries['css']['autofill']        = '//cdn.datatables.net/autofill/'.$versions['AutoFill']['release']['version'].'/css/dataTables.autoFill.css';
 	DT_Example::$lookup_libraries['js' ]['autofill']        = '//cdn.datatables.net/autofill/'.$versions['AutoFill']['release']['version'].'/js/dataTables.autoFill.min.js';
+	DT_Example::$lookup_libraries['css']['buttons']         = '//cdn.datatables.net/buttons/'.$versions['Buttons']['release']['version'].'/css/buttons.dataTables.css';
+	DT_Example::$lookup_libraries['js' ]['buttons']         = '//cdn.datatables.net/buttons/'.$versions['Buttons']['release']['version'].'/js/dataTables.buttons.min.js';
 	DT_Example::$lookup_libraries['css']['colreorder']      = '//cdn.datatables.net/colreorder/'.$versions['ColReorder']['release']['version'].'/css/dataTables.colReorder.css';
 	DT_Example::$lookup_libraries['js' ]['colreorder']      = '//cdn.datatables.net/colreorder/'.$versions['ColReorder']['release']['version'].'/js/dataTables.colReorder.min.js';
 	DT_Example::$lookup_libraries['css']['colvis']          = '//cdn.datatables.net/colvis/'.$versions['ColVis']['release']['version'].'/css/dataTables.colVis.css';
@@ -117,6 +119,8 @@ if ( isset( $options['cdn'] ) ) {
 	DT_Example::$lookup_libraries['js' ]['responsive']      = '//cdn.datatables.net/responsive/'.$versions['Responsive']['release']['version'].'/js/dataTables.responsive.min.js';
 	DT_Example::$lookup_libraries['css']['scroller']        = '//cdn.datatables.net/scroller/'.$versions['Scroller']['release']['version'].'/css/dataTables.scroller.css';
 	DT_Example::$lookup_libraries['js' ]['scroller']        = '//cdn.datatables.net/scroller/'.$versions['Scroller']['release']['version'].'/js/dataTables.scroller.min.js';
+	DT_Example::$lookup_libraries['css']['select']          = '//cdn.datatables.net/select/'.$versions['Select']['release']['version'].'/css/select.dataTables.css';
+	DT_Example::$lookup_libraries['js' ]['select']          = '//cdn.datatables.net/select/'.$versions['Select']['release']['version'].'/js/dataTables.select.min.js';
 	DT_Example::$lookup_libraries['css']['tabletools']      = '//cdn.datatables.net/tabletools/'.$versions['TableTools']['release']['version'].'/css/dataTables.tableTools.css';
 	DT_Example::$lookup_libraries['js' ]['tabletools']      = '//cdn.datatables.net/tabletools/'.$versions['TableTools']['release']['version'].'/js/dataTables.tableTools.min.js';
 
@@ -133,6 +137,8 @@ else {
 	DT_Example::$lookup_libraries['js' ]['datatables']      = $dir_media.'/js/jquery.dataTables.js';
 	DT_Example::$lookup_libraries['css']['autofill']        = path_simplify( $dir_media.'/../extensions/AutoFill/css/dataTables.autoFill.css' );
 	DT_Example::$lookup_libraries['js' ]['autofill']        = path_simplify( $dir_media.'/../extensions/AutoFill/js/dataTables.autoFill.js' );
+	DT_Example::$lookup_libraries['css']['buttons']         = path_simplify( $dir_media.'/../extensions/Buttons/css/buttons.dataTables.css' );
+	DT_Example::$lookup_libraries['js' ]['buttons']         = path_simplify( $dir_media.'/../extensions/Buttons/js/dataTables.buttons.js' );
 	DT_Example::$lookup_libraries['css']['colreorder']      = path_simplify( $dir_media.'/../extensions/ColReorder/css/dataTables.colReorder.css' );
 	DT_Example::$lookup_libraries['js' ]['colreorder']      = path_simplify( $dir_media.'/../extensions/ColReorder/js/dataTables.colReorder.js' );
 	DT_Example::$lookup_libraries['css']['colvis']          = path_simplify( $dir_media.'/../extensions/ColVis/css/dataTables.colVis.css' );
@@ -148,6 +154,8 @@ else {
 	DT_Example::$lookup_libraries['js' ]['responsive']      = path_simplify( $dir_media.'/../extensions/Responsive/js/dataTables.responsive.js' );
 	DT_Example::$lookup_libraries['css']['scroller']        = path_simplify( $dir_media.'/../extensions/Scroller/css/dataTables.scroller.css' );
 	DT_Example::$lookup_libraries['js' ]['scroller']        = path_simplify( $dir_media.'/../extensions/Scroller/js/dataTables.scroller.js' );
+	DT_Example::$lookup_libraries['css']['select']          = path_simplify( $dir_media.'/../extensions/Select/css/select.dataTables.css' );
+	DT_Example::$lookup_libraries['js' ]['select']          = path_simplify( $dir_media.'/../extensions/Select/js/dataTables.select.js' );
 	DT_Example::$lookup_libraries['css']['tabletools']      = path_simplify( $dir_media.'/../extensions/TableTools/css/dataTables.tableTools.css' );
 	DT_Example::$lookup_libraries['js' ]['tabletools']      = path_simplify( $dir_media.'/../extensions/TableTools/js/dataTables.tableTools.js' );
 	
