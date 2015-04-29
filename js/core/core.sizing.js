94636f8dc3cc2d44f2621601c745ab0f7db04edd
Allan Jardine <allan.jardine@sprymedia.co.uk>
2015-04-29 21:17:50 +0100

    Fix: IE6/7 could crash when loading a DataTable on page load due to the binding of a `resize` event listener on the window object
    * Reported in thread 27428
    * This SO thread was helpful: http://stackoverflow.com/questions/1264443

--- js/core/core.sizing.js
+++ js/core/core.sizing.js
@@ -169,9 +169,20 @@ function _fnCalculateColumnWidths ( oSettings )
 	}
 
 	if ( (tableWidthAttr || scrollX) && ! oSettings._reszEvt ) {
-		$(window).bind('resize.DT-'+oSettings.sInstance, _fnThrottle( function () {
-			_fnAdjustColumnSizing( oSettings );
-		} ) );
+		var bindResize = function () {
+			$(window).bind('resize.DT-'+oSettings.sInstance, _fnThrottle( function () {
+				_fnAdjustColumnSizing( oSettings );
+			} ) );
+		};
+
+		// IE6/7 will crash if we bind a resize event handler on page load.
+		// To be removed in 1.11 which drops IE6/7 support
+		if ( oSettings.oBrowser.bScrollOversize ) {
+			setTimeout( bindResize, 1000 );
+		}
+		else {
+			bindResize();
+		}
 
 		oSettings._reszEvt = true;
 	}
