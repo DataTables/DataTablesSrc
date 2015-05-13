1141742b4154d48278ef2cfc5fb4d3646ad7a87a
Allan Jardine <allan.jardine@sprymedia.co.uk>
2015-05-13 13:49:59 +0100

    New: Add `numbers` paging option for `dt-init pagingType` to round off the full set of basic options
    Thank you `rubenduiveman` for suggesting this enhancement

--- js/ext/ext.paging.js
+++ js/ext/ext.paging.js
@@ -44,6 +44,10 @@ $.extend( extPagination, {
 		return [  'first', 'previous', 'next', 'last' ];
 	},
 
+	numbers: function ( page, pages ) {
+		return [ _numbers(page, pages) ];
+	},
+
 	simple_numbers: function ( page, pages ) {
 		return [ 'previous', _numbers(page, pages), 'next' ];
 	},
