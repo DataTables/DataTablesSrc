7e8f65896fcb309feb2e96f4bd531183d907a69b
Allan Jardine <allan.jardine@sprymedia.co.uk>
2015-07-03 15:38:49 +0100

    Fix: Row details could potentially throw an error if used on an row that doesn't exist (might have been removed)

--- js/api/api.row.details.js
+++ js/api/api.row.details.js
@@ -52,7 +52,7 @@ var __details_remove = function ( api, idx )
 	if ( ctx.length ) {
 		var row = ctx[0].aoData[ idx !== undefined ? idx : api[0] ];
 
-		if ( row._details ) {
+		if ( row && row._details ) {
 			row._details.remove();
 
 			row._detailsShow = undefined;
