DataTables/DataTablesSrc

commit 67ed583159419cec6fee14c5ecaafc5f75e7cf5c

Fix: `dt-init scrollX` could not be given as `false` - it still enabled x-scrolling

js/core/core.compat.js
@@ -149,6 +149,11 @@ function _fnCompatOpts ( init )
 	_fnCompatMap( init, 'pageLength',    'iDisplayLength' );
 	_fnCompatMap( init, 'searching',     'bFilter' );
 
+	// Boolean initialisation of x-scrolling
+	if ( typeof init.sScrollX === 'boolean' ) {
+		init.sScrollX = init.sScrollX ? '100%' : '';
+	}
+
 	// Column search objects are in an array, so it needs to be converted
 	// element by element
 	var searchCols = init.aoSearchCols;

js/core/core.constructor.js
@@ -204,9 +204,6 @@ if ( oSettings.oScroll.sX !== "" || oSettings.oScroll.sY !== "" )
 {
 	oSettings.oScroll.iBarWidth = _fnScrollBarWidth();
 }
-if ( oSettings.oScroll.sX === true ) { // Easy initialisation of x-scrolling
-	oSettings.oScroll.sX = '100%';
-}
 
 if ( oSettings.iInitDisplayStart === undefined )
 {

commit 9d55a24e0f9f7ae15cb34a2f7119368ddf6043f1

Fix: If x-scrolling is enabled using `$.fn.dataTable.defaults.scrollX=true;` an error would occur
- Fixes thread 30084
- Error introduced by commit 67ed583159419cec6fee14c5ecaafc5f75e7cf5c

js/core/core.compat.js
@@ -153,6 +153,9 @@ function _fnCompatOpts ( init )
 	if ( typeof init.sScrollX === 'boolean' ) {
 		init.sScrollX = init.sScrollX ? '100%' : '';
 	}
+	if ( typeof init.scrollX === 'boolean' ) {
+		init.scrollX = init.scrollX ? '100%' : '';
+	}
 
 	// Column search objects are in an array, so it needs to be converted
 	// element by element