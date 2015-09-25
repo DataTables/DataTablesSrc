5d4410631322105b5c8e934801c355b3f0153869
Mont Rothstein <mont.rothstein@gmail.com>
2015-09-25 09:55:25 -0700

    Added hold-order option to draw() and ignoreSortAndFilter flag to _fnReDraw() so that data may be updated without re-flowing the table.

--- js/api/api.draw.js
+++ js/api/api.draw.js
@@ -8,6 +8,11 @@ _api_register( 'draw()', function ( paging ) {
 		if ( paging === 'page' ) {
 			_fnDraw( settings );
 		}
+		// To update the data without re-sorting, re-filtering, or changing the page, the hold-order option was added.
+		// This sets the ignoreSortAndFilter falg on _fnReDraw.
+		else if (paging === 'hold-order') {
+					_fnReDraw(settings, true, true);
+		}
 		else {
 			if ( typeof paging === 'string' ) {
 				paging = paging === 'full-hold' ?
