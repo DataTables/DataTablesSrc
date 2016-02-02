6fe17f5956d8c318686798fbd797b2d092366005
Allan Jardine <allan.jardine@sprymedia.co.uk>
2016-02-02 13:15:49 +0000

    Fix: Javascript error would occur if trying to use `dt-api cells().nodes()` on a cell that doesn't exisit in the DataTable
    - Fixes DataTables/KeyTable #23.

--- js/api/api.cells.js
+++ js/api/api.cells.js
@@ -127,9 +127,10 @@ _api_register( 'cells()', function ( rowSelector, columnSelector, opts ) {
 
 _api_registerPlural( 'cells().nodes()', 'cell().node()', function () {
 	return this.iterator( 'cell', function ( settings, row, column ) {
-		var cells = settings.aoData[ row ].anCells;
-		return cells ?
-			cells[ column ] :
+		var data = settings.aoData[ row ];
+
+		return data && data.anCells ?
+			data.anCells[ column ] :
 			undefined;
 	}, 1 );
 } );
