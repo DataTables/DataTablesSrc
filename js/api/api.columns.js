7925dbe9b9a30dd339898670926e3fe89cd2db15
Allan Jardine <allan.jardine@sprymedia.co.uk>
2016-04-19 16:42:30 +0100

    Dev: Fix recursion error with Responsive from last commit. Need to be more selective about when we fire `column-visiblity`!

--- js/api/api.columns.js
+++ js/api/api.columns.js
@@ -240,13 +240,15 @@ _api_registerPlural( 'columns().visible()', 'column().visible()', function ( vis
 	} );
 
 	// Group the column visibility changes
-	if ( vis !== undefined && ( calc === undefined || calc ) ) {
-		this.columns.adjust();
-
-		// Fire the column visibility event after any recalc is done
+	if ( vis !== undefined ) {
+		// Second loop once the first is done for events
 		this.iterator( 'column', function ( settings, column ) {
 			_fnCallbackFire( settings, null, 'column-visibility', [settings, column, vis, calc] );
 		} );
+
+		if ( calc === undefined || calc ) {
+			this.columns.adjust();
+		}
 	}
 
 	return ret;
