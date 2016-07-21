0ef6f5b1bf9c175666a9b3b0dc5204c35e33499f
Allan Jardine <allan.jardine@sprymedia.co.uk>
2016-07-21 15:08:47 +0100

    Dev: Revert 6f6b3d4b14fbba4a8818d8a24dc71763f9e92c80 - it was completely wrong!

--- js/ext/ext.renderer.js
+++ js/ext/ext.renderer.js
@@ -8,7 +8,7 @@ $.extend( true, DataTable.ext.renderer, {
 			// `DT` namespace will allow the event to be removed automatically
 			// on destroy, while the `dt` namespaced event is the one we are
 			// listening for
-			$(settings.nTable).on( 'order.dt.DT', function ( e, ctx, column, classes ) {
+			$(settings.nTable).on( 'order.dt.DT', function ( e, ctx, sorting, columns ) {
 				if ( settings !== ctx ) { // need to check this this is the host
 					return;               // table, not a nested one
 				}
@@ -21,8 +21,8 @@ $.extend( true, DataTable.ext.renderer, {
 						classes.sSortAsc +' '+
 						classes.sSortDesc
 					)
-					.addClass( classes[ colIdx ] == 'asc' ?
-						classes.sSortAsc : classes[ colIdx ] == 'desc' ?
+					.addClass( columns[ colIdx ] == 'asc' ?
+						classes.sSortAsc : columns[ colIdx ] == 'desc' ?
 							classes.sSortDesc :
 							column.sSortingClass
 					);
@@ -39,7 +39,7 @@ $.extend( true, DataTable.ext.renderer, {
 				.appendTo( cell );
 
 			// Attach a sort listener to update on sort
-			$(settings.nTable).on( 'order.dt.DT', function ( e, ctx, column, classes ) {
+			$(settings.nTable).on( 'order.dt.DT', function ( e, ctx, sorting, columns ) {
 				if ( settings !== ctx ) {
 					return;
 				}
@@ -48,8 +48,8 @@ $.extend( true, DataTable.ext.renderer, {
 
 				cell
 					.removeClass( classes.sSortAsc +" "+classes.sSortDesc )
-					.addClass( classes[ colIdx ] == 'asc' ?
-						classes.sSortAsc : classes[ colIdx ] == 'desc' ?
+					.addClass( columns[ colIdx ] == 'asc' ?
+						classes.sSortAsc : columns[ colIdx ] == 'desc' ?
 							classes.sSortDesc :
 							column.sSortingClass
 					);
@@ -63,8 +63,8 @@ $.extend( true, DataTable.ext.renderer, {
 						classes.sSortJUIAscAllowed +" "+
 						classes.sSortJUIDescAllowed
 					)
-					.addClass( classes[ colIdx ] == 'asc' ?
-						classes.sSortJUIAsc : classes[ colIdx ] == 'desc' ?
+					.addClass( columns[ colIdx ] == 'asc' ?
+						classes.sSortJUIAsc : columns[ colIdx ] == 'desc' ?
 							classes.sSortJUIDesc :
 							column.sSortingClassJUI
 					);
