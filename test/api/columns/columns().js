f2f232921d12abce0cb8e6db64eae0e3ce58f008
tombuckle-uk <13012481@uhi.ac.uk>
2016-09-08 15:48:25 +0100

    Added tests for column() and columns() api

--- test/api/columns/columns().js
+++ test/api/columns/columns().js
@@ -34,7 +34,7 @@
 //   - Can select columns using nodes with `search: current` - confirm expected data
 
 
-describe( 'columns- columns()', function() {
+describe( 'columns- columns() -solo', function() {
 	var table;
 
 	dt.libs( {
@@ -63,4 +63,214 @@ describe( 'columns- columns()', function() {
 		expect( cols.eq(2).text() ).toBe( 'Age' );
 		expect( cols.eq(3).text() ).toBe( 'Start date' );
 	} );
+	dt.html( 'basic' );
+	it("Exists and is a function", function () {
+		var table = $('#example').DataTable();
+		expect(typeof table.columns).toBe('function');
+	});
+	dt.html( 'basic' );
+	it("Returns an API instance", function () {
+		var table = $('#example').DataTable();
+		expect(table.columns() instanceof $.fn.dataTable.Api).toBe(true);
+	});
+	//No arguments
+	dt.html( 'basic' );
+	it("No arguments- will select all columns", function () {
+		var table = $('#example').DataTable();
+		var result = table.columns();
+		expect(result[0]).toEqual([0, 1, 2, 3, 4, 5]);
+	});
+	dt.html( 'basic' );
+	it("No arguments- hide first column, still selects all columns", function () {
+		var table = $('#example').DataTable({
+			"columnDefs": [
+				{
+					"targets": [ 0 ],
+					"visible": false
+				}
+			]
+		});
+		var result = table.columns();
+		expect(result[0]).toEqual([0, 1, 2, 3, 4, 5]);
+	});
+
+	dt.html( 'basic' );
+	it("Single arguments-  Can select all columns with '*'", function () {
+		var table = $('#example').DataTable();
+		var result = table.columns('*');
+		expect(result[0]).toEqual([0, 1, 2, 3, 4, 5]);
+	});
+
+	dt.html( 'basic' );
+	it("Single arguments-  Can select a single column using an integer", function () {
+		var table = $('#example').DataTable();
+		var result = table.columns(1);
+		expect(result[0][0]).toBe(1);
+	});
+
+	dt.html( 'basic' );
+	it("Single arguments-  Can select a column based on the visible index", function () {
+		var table = $('#example').DataTable({
+			"columnDefs": [
+				{
+					"targets": [ 1 ],
+					"visible": false
+				}
+			]
+		});
+		var result = table.columns('0:visIdx');
+		var result2 = table.columns('1:visIdx');
+		expect(result[0][0]).toBe(0);
+		expect(result2[0][0]).toBe(2);
+	});
+	dt.html( 'basic' );
+	it("Single arguments-  Can select a column based on the visible index- test 2", function () {
+		var table = $('#example').DataTable({
+			"columnDefs": [
+				{
+					"targets": [ 1 ],
+					"visible": false
+				}
+			]
+		});
+		var result = table.columns('0:visible');
+		var result2 = table.columns('1:visible');
+
+		expect(result[0][0]).toBe(0);
+		expect(result2[0][0]).toBe(2);
+	});
+	dt.html( 'basic' );
+	it("Single arguments-  Can select a column name selector", function () {
+		var table = $('#example').DataTable({
+			"columnDefs": [
+				{ "name": "Name",   "targets": 0 }
+			]
+		});
+		var result = table.columns('Name:name');
+		expect(result[0][0]).toBe(0);
+	});
+
+	dt.html( 'basic' );
+	it("Single arguments-  Can select a column 'jQuery' classname selector", function () {
+		var table = $('#example').DataTable({
+			"columnDefs": [
+				{ "name": "Name",   "targets": 0 }
+			]
+		});
+		var result = table.columns('.sorting_asc');
+		expect(result[0][0]).toBe(0);
+	});
+
+	dt.html( 'basic' );
+	it("Single arguments-  Can select a column header node", function () {
+		var table = $('#example').DataTable({
+			"columnDefs": [
+				{ "name": "Name",   "targets": 0 }
+			]
+		});
+		var result = table.column('#example thead th:eq(0)');
+		expect(result[0][0]).toBe(0);
+	});
+
+	dt.html( 'basic' );
+	it("Single arguments-  Can select a column using function", function () { // need help with this
+		var table = $('#example').DataTable({
+			"columnDefs": [
+				{ "name": "Name",   "targets": 0 }
+			]
+		});
+		var result = table.columns(function(idx, data, node){
+			return '#example thead th:eq(0)';
+		});
+		var result2 = table.columns(function(idx, data, node){
+			return '#example thead th:eq(3)';
+		});
+		expect(result[0][0]).toBe(0);
+	});
+
+	dt.html( 'basic' );
+	it("Single argument- modifier-  Can select a column header node", function () {
+		var table = $('#example').DataTable({
+			"columnDefs": [
+				{ "name": "Name",   "targets": 0 }
+			]
+		});
+		var result = table.columns({order:'current'} ).data();
+		expect(result[0][0]).toBe("Airi Satou");
+	});
+	dt.html( 'basic' );
+	it("Single argument- modifier- 'order: index'", function () {
+		var table = $('#example').DataTable({
+			"columnDefs": [
+				{ "name": "Name",   "targets": 0 }
+			]
+		});
+		var result = table.columns({order:'index'} ).data();
+		expect(result[0][0]).toBe("Tiger Nixon");
+	});
+	dt.html( 'basic' );
+	it("Single argument- modifier-  'order: current'", function () {
+		var table = $('#example').DataTable({
+			"columnDefs": [
+				{ "name": "Name",   "targets": 0 }
+			]
+		});
+		var result = table.columns({order:'current'} ).data();
+		expect(result[0][0]).toBe("Airi Satou");
+	});
+	dt.html( 'basic' );
+	it("Single argument- modifier-  'page: current' & Correct length", function () {
+		var table = $('#example').DataTable({
+			"columnDefs": [
+				{ "name": "Name",   "targets": 0 }
+			]
+		});
+		var result = table.columns({page:'current'} ).data();
+		// console.log(Object.keys(result).length);  //Ask about this. How to get length of data not the whole object.
+		// console.log(result);
+		expect(result[0][0]).toBe("Airi Satou");
+	});
+	dt.html( 'basic' );
+	it("Single argument- modifier- 'search: applied' Applied search, only filtered data is returned", function () {
+		var table = $('#example').DataTable({
+			"columnDefs": [
+				{ "name": "Name",   "targets": 0 }
+			]
+		});
+		$('#example_filter input').val('Accountant').keyup();
+		var result = table.columns({search:'applied'} ).data(); //There is no 'search: current' used applied instead
+		expect(result[0][0]).toBe("Airi Satou");
+		expect(result[0][1]).toBe("Garrett Winters");
+	});
+			dt.html( 'basic' );
+	it("Two arguments - selector and modifier- Can select single columns using index with `order: applied` - confirm data is in expected order", function () {
+		var table = $('#example').DataTable({
+			"columnDefs": [
+				{ "name": "Name",   "targets": 0 }
+			]
+		});
+		var result = table.columns( 0, {order:'applied'} ).data();
+		expect(result[0][0]).toBe("Airi Satou");
+	});
+	dt.html( 'basic' );
+	it("Two arguments - selector and modifier-  - Can select column using a class selector and `page: current` confirm expected data", function () {
+		var table = $('#example').DataTable({
+			"columnDefs": [
+				{ "name": "Name",   "targets": 0 }
+			]
+		});
+		var result = table.columns( '.sorting_asc', {page:'current'} ).data();
+		expect(result[0][0]).toBe("Airi Satou");
+	});
+	dt.html( 'basic' );
+	it("Two arguments - selector and modifier- Can select column using node with `search: current` - confirm expected data", function () {
+		var table = $('#example').DataTable({
+			"columnDefs": [
+				{ "name": "Name",   "targets": 0 }
+			]
+		});
+		$('#example_filter input').val('Accountant').keyup();
+		var result = table.columns( '#example thead th:eq(0)', {search:'applied'} ).data();
+		expect(result[0][0]).toBe("Airi Satou");
+	});
 } );
