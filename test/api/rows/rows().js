DataTables/DataTablesSrc

commit dfbf4fa23aa0952ee007bca06b791c2cd0ad48bb

Fix: Row selector would incorrectly select rows if row index 0 was passed in with a selector modifier.
- See thread 37834. Much qudos to `guyclairbois` for spotting this and
  test case!

js/api/api.rows.js
@@ -34,7 +34,7 @@ var __row_selector = function ( settings, selector, opts )
 			// Selector - integer
 			return [ selInt ];
 		}
-		else if ( ! sel ) {
+		else if ( sel === null || sel === undefined ) {
 			// Selector - none
 			return rows;
 		}

test/api/rows/row().js
@@ -1,19 +1,32 @@
-// todo tests
-// 1- Select row, make sure returned row is correct, test using integet(rowindex) and id selector
-// 2- Use modifer to change order upon use, also could test with a search applied
+// todo tests - write the tests...
 
+describe( 'rows: cells()', function() {
+	var table;
 
-describe( "rows- row()", function() {
 	dt.libs( {
 		js:  [ 'jquery', 'datatables' ],
 		css: [ 'datatables' ]
 	} );
 
-	describe("Check the defaults", function () {
-		dt.html( 'basic' );
-		it("Default should be null", function () {
-				});
+	dt.html( 'basic' );
 
-	});
+	it ( 'modifier - row index of 1, no data match' , function () {
+		table = $('#example').DataTable()
 
-});
+		// This will leave row index 4 in the display
+		table.search( 'air' ).draw();
+
+		var d = table.row( 1, { search: 'applied' } ).data();
+		expect( d ).toBe( undefined );
+	} );
+
+	it ( 'modifier - row index of 0, no data match' , function () {
+		var d = table.row( 0, { search: 'applied' } ).data();
+		expect( d ).toBe( undefined );
+	} );
+
+	it ( 'modifier - row index of 4, data match' , function () {
+		var d = table.row( 4, { search: 'applied' } ).data();
+		expect( d[0] ).toBe( 'Airi Satou' );
+	} );
+} );

commit 92dbf417ae3c326da5acc60ccb39a12166959f8c

Dev: Fix no selector issue introduced by dfbf4fa23aa0952ee007bca06b791c2cd0ad48bb

js/api/api.rows.js
@@ -34,7 +34,7 @@ var __row_selector = function ( settings, selector, opts )
 			// Selector - integer
 			return [ selInt ];
 		}
-		else if ( sel === null || sel === undefined ) {
+		else if ( sel === null || sel === undefined || sel === '' ) {
 			// Selector - none
 			return rows;
 		}

test/api/rows/row().js
@@ -1,6 +1,6 @@
 // todo tests - write the tests...
 
-describe( 'rows: cells()', function() {
+describe( 'rows: row()', function() {
 	var table;
 
 	dt.libs( {

test/api/rows/rows().js
@@ -1,20 +1,32 @@
-// todo tests
-// 1- Select rows, make sure returned rows are correct, test using integer(rowindex) and id selector
-// 2- Use modifer to change order upon use, also could test with a search applied
+// todo tests - write the tests...
 
+describe( 'rows: rows()', function() {
+	var table;
 
-
-describe( "rows- rows()", function() {
 	dt.libs( {
 		js:  [ 'jquery', 'datatables' ],
 		css: [ 'datatables' ]
 	} );
 
-	describe("Check the defaults", function () {
-		dt.html( 'basic' );
-		it("Default should be null", function () {
-				});
+	dt.html( 'basic' );
 
-	});
+	it ( 'select all rows - no selector' , function () {
+		table = $('#example').DataTable()
+		expect( table.rows().count() ).toBe( 57 );
+	} );
 
-});
+	it ( 'select all rows - undefined selector' , function () {
+		table = $('#example').DataTable()
+		expect( table.rows( undefined ).count() ).toBe( 57 );
+	} );
+
+	it ( 'select all rows - null selector' , function () {
+		table = $('#example').DataTable()
+		expect( table.rows( null ).count() ).toBe( 57 );
+	} );
+
+	it ( 'select one row - 0 selector' , function () {
+		table = $('#example').DataTable()
+		expect( table.rows( 0 ).count() ).toBe( 1 );
+	} );
+} );