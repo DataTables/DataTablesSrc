5300387cfec28cc7f0182166894833583cdcfda4
Allan Jardine <allan.jardine@sprymedia.co.uk>
2017-04-14 21:09:46 +0100

    Fix: API performance improvement - sigifnicantly faster API access for large tables. Testing with a table of 50'000 rows `rows().count()` is now 23mS executation time, while it was 996mS before!
    - The `_unique()` function was a killer because it uses nested loops to
      determine if items have been seen before in an array. In the majority
      of cases when accessing the API the items will be unique already, so
      running the nested loops is hugely detremental to performance.
    - Rather than attempting to improve the performance of `_unique()`
      itself, we try to avoid running its algorithim at all. An array of the
      items is sorted and tested to see if there are unique items or not. In
      the majority of cases this means a massive performance boost since it
      is a single loop rather than a nested loop.

--- test/api/rows/rows().js
+++ test/api/rows/rows().js
@@ -16,17 +16,22 @@ describe( 'rows: rows()', function() {
 	} );
 
 	it ( 'select all rows - undefined selector' , function () {
-		table = $('#example').DataTable()
 		expect( table.rows( undefined ).count() ).toBe( 57 );
 	} );
 
 	it ( 'select all rows - null selector' , function () {
-		table = $('#example').DataTable()
 		expect( table.rows( null ).count() ).toBe( 57 );
 	} );
 
 	it ( 'select one row - 0 selector' , function () {
-		table = $('#example').DataTable()
 		expect( table.rows( 0 ).count() ).toBe( 1 );
+	} );
+
+	it ( 'Select nodes by `tr` selector' , function () {
+		expect( table.rows( 'tr' ).count() ).toBe( 57 );
+	} );
+
+	it ( 'Selector will gather rows uniquely' , function () {
+		expect( table.rows( ['tr', 'tr'] ).count() ).toBe( 57 );
 	} );
 } );
