b146257fb8ed802048f67de1562723915e586979
Marcel Link <marcel.link@t-online.de>
2017-11-11 03:16:02 +0100

    Fix: util.throttle misbehavior

--- js/api/api.util.js
+++ js/api/api.util.js
@@ -21,26 +21,27 @@ DataTable.util = {
 	throttle: function ( fn, freq ) {
 		var
 			frequency = freq !== undefined ? freq : 200,
-			last,
+			call,
+			that,
 			timer;
 
+		function timeout() {
+			fn.apply( that, call );
+			call = undefined;
+			that = undefined;
+			timer = setTimeout( function () {
+				timer = undefined;
+				if ( call ) {
+					timeout();
+				}
+			}, frequency );
+		}
+
 		return function () {
-			var
-				that = this,
-				now  = +new Date(),
-				args = arguments;
-
-			if ( last && now < last + frequency ) {
-				clearTimeout( timer );
-
-				timer = setTimeout( function () {
-					last = undefined;
-					fn.apply( that, args );
-				}, frequency );
-			}
-			else {
-				last = now;
-				fn.apply( that, args );
+			that = this;
+			call = arguments;
+			if ( !timer ) {
+				timeout();
 			}
 		};
 	},
