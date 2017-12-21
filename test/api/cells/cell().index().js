a44b2169224078fcabc7b14144acfba89b4cf4b2
Colin Marks <colin@datatables.net>
2017-12-21 11:23:38 +0000

    dev added new tests for cell().index() and minor tweaks to documentation

--- test/api/cells/cell().index().js
+++ test/api/cells/cell().index().js
@@ -1,19 +1,86 @@
-// todo tests
-// - Confirm it exists and is a function
-// - Make sure row,column and visible column index info is being returned. and they are all integers
-// - Hide column index 1 using `column().visible()` and check the index information for cells in columns 0, 1 and 2.
-// - Can use the `row` index as a selector for `row()` (perhaps check with `row().data()`.
-describe( "cells- cell().index()", function() {
-	dt.libs( {
-		js:  [ 'jquery', 'datatables' ],
-		css: [ 'datatables' ]
-	} );
-
-	describe("Check the defaults", function () {
-		dt.html( 'basic' );
-		it("Default should be null", function () {
-				});
-
+describe('cells - cell().index()', function() {
+	dt.libs({
+		js: ['jquery', 'datatables'],
+		css: ['datatables']
 	});
 
+	describe('Check the defaults', function() {
+		dt.html('basic');
+
+		it('Exists and is a function', function() {
+			var table = $('#example').DataTable();
+			expect(typeof table.cell().index).toBe('function');
+		});
+
+		it('Returns an object of Object type containing three integers', function() {
+			var table = $('#example').DataTable();
+			var index = table.cell().index();
+
+			expect(Object.keys(index).length).toBe(3);
+
+			expect(typeof index.row).toBe('number');
+			expect(typeof index.column).toBe('number');
+			expect(typeof index.columnVisible).toBe('number');
+		});
+	});
+
+	describe('Returns correct information for hidden columns', function() {
+		dt.html('basic');
+
+		it('Returns correct information before hidden column', function() {
+			var table = $('#example').DataTable();
+
+			table.column(1).visible(false);
+
+			var index = table.cell(0, 0).index();
+
+			expect(index.row).toBe(0);
+			expect(index.column).toBe(0);
+			expect(index.columnVisible).toBe(0);
+		});
+
+		dt.html('basic');
+
+		it('Returns correct information for hidden column', function() {
+			var table = $('#example').DataTable();
+
+			table.column(1).visible(false);
+
+			var index = table.cell(0, 1).index();
+
+			expect(index.row).toBe(0);
+			expect(index.column).toBe(1);
+			expect(index.columnVisible).toBe(null);
+		});
+
+		dt.html('basic');
+
+		it('Returns correct information after hidden column', function() {
+			var table = $('#example').DataTable();
+
+			table.column(1).visible(false);
+
+			var index = table.cell(0, 2).index();
+
+			expect(index.row).toBe(0);
+			expect(index.column).toBe(2);
+			expect(index.columnVisible).toBe(1);
+		});
+	});
+
+	describe('Returns correct row and column information', function() {
+		dt.html('basic');
+
+		it('Returns correct row information', function() {
+			var table = $('#example').DataTable();
+
+			expect(table.row(table.cell(2, 3).index().row).data()[0]).toBe('Ashton Cox');
+		});
+
+		it('Returns correct column information', function() {
+			var table = $('#example').DataTable();
+
+			expect(table.column(table.cell(2, 3).index().column).data()[0]).toBe('33');
+		});
+	});
 });
