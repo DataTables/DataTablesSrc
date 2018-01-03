2eb6e6d14aa6df1a8dcc38869ac53502e4201414
Colin Marks <colin@datatables.net>
2018-01-03 20:18:07 +0000

    dev continuing with extending the unit tests - mostly around the index and indexes methods

--- test/api/cells/cells().indexes().js
+++ test/api/cells/cells().indexes().js
@@ -1,21 +1,73 @@
 // todo tests
-// - Confirm it exists and is a function
-// - Confirm it returns an API instance
-// - For each cell selected
-//   - Make sure row,column and visible column index info is being returned. and they are all integers
-// - Hide column index 1 using `column().visible()` and check the index information for cells in columns 0, 1 and 2 (using a single selector to get them)
 // - Create a new API instance with the API with the indexes passed in as the argument to the API constructor
-describe( "cells- cells().indexes()", function() {
-	dt.libs( {
-		js:  [ 'jquery', 'datatables' ],
-		css: [ 'datatables' ]
-	} );
-
-	describe("Check the defaults", function () {
-		dt.html( 'basic' );
-		it("Default should be null", function () {
-				});
-
+describe('cells - cells().indexes()', function() {
+	dt.libs({
+		js: ['jquery', 'datatables'],
+		css: ['datatables']
 	});
 
+	describe('Check the defaults', function() {
+		dt.html('basic');
+
+		it('Exists and is a function', function() {
+			let table = $('#example').DataTable();
+			expect(typeof table.cells().indexes).toBe('function');
+		});
+
+		it('Returns an API instance', function() {
+			let table = $('#example').DataTable();
+			expect(table.cells().indexes() instanceof $.fn.dataTable.Api).toBe(true);
+		});
+
+		it('Returns an object containing three integers', function() {
+			let table = $('#example').DataTable();
+			let cellIdx = table.cells(0, 0).indexes()[0];
+
+			expect(Object.keys(cellIdx).length).toBe(3);
+
+			expect(Number.isInteger(cellIdx.row)).toBe(true);
+			expect(Number.isInteger(cellIdx.column)).toBe(true);
+			expect(Number.isInteger(cellIdx.columnVisible)).toBe(true);
+		});
+	});
+
+	describe('Check behaviour', function() {
+		dt.html('basic');
+
+		it('Correct cell indexes when no columns hidden', function() {
+			let table = $('#example').DataTable();
+
+			$(table.row(2).nodes()).addClass('selected');
+			let cellIdx = table.cells('.selected', '*').indexes();
+
+			expect(cellIdx.length).toBe(6);
+
+			for (let i = 0; i < 6; i++) {
+				expect(cellIdx[i].row).toBe(2);
+				expect(cellIdx[i].column).toBe(i);
+				expect(cellIdx[i].columnVisible).toBe(i);
+			}
+		});
+
+		dt.html('basic');
+
+		it('Correct indexes if one column is hidden', function() {
+			let table = $('#example').DataTable();
+			$(table.row(2).nodes()).addClass('selected');
+			table.column(1).visible(false);
+
+			let cellIdx = table.cells('.selected', '*').indexes();
+			let columnVisible = null;
+
+			expect(cellIdx.length).toBe(6);
+
+			for (let i = 0; i < 6; i++) {
+				expect(cellIdx[i].row).toBe(2);
+				expect(cellIdx[i].column).toBe(i);
+
+				columnVisible = i == 1 ? null : i < 1 ? i : i - 1;
+				expect(cellIdx[i].columnVisible).toBe(columnVisible);
+			}
+		});
+	});
 });
--- test/api/rows/rows().js
+++ test/api/rows/rows().js
@@ -1,37 +1,37 @@
 // todo tests - write the tests...
 
-describe( 'rows: rows()', function() {
+describe('rows: rows()', function() {
 	var table;
 
-	dt.libs( {
-		js:  [ 'jquery', 'datatables' ],
-		css: [ 'datatables' ]
-	} );
+	dt.libs({
+		js: ['jquery', 'datatables'],
+		css: ['datatables']
+	});
 
-	dt.html( 'basic' );
+	dt.html('basic');
 
-	it ( 'select all rows - no selector' , function () {
-		table = $('#example').DataTable()
-		expect( table.rows().count() ).toBe( 57 );
-	} );
+	it('select all rows - no selector', function() {
+		table = $('#example').DataTable();
+		expect(table.rows().count()).toBe(57);
+	});
 
-	it ( 'select all rows - undefined selector' , function () {
-		expect( table.rows( undefined ).count() ).toBe( 57 );
-	} );
+	it('select all rows - undefined selector', function() {
+		expect(table.rows(undefined).count()).toBe(57);
+	});
 
-	it ( 'select all rows - null selector' , function () {
-		expect( table.rows( null ).count() ).toBe( 57 );
-	} );
+	it('select all rows - null selector', function() {
+		expect(table.rows(null).count()).toBe(57);
+	});
 
-	it ( 'select one row - 0 selector' , function () {
-		expect( table.rows( 0 ).count() ).toBe( 1 );
-	} );
+	it('select one row - 0 selector', function() {
+		expect(table.rows(0).count()).toBe(1);
+	});
 
-	it ( 'Select nodes by `tr` selector' , function () {
-		expect( table.rows( 'tr' ).count() ).toBe( 57 );
-	} );
+	it('Select nodes by `tr` selector', function() {
+		expect(table.rows('tr').count()).toBe(57);
+	});
 
-	it ( 'Selector will gather rows uniquely' , function () {
-		expect( table.rows( ['tr', 'tr'] ).count() ).toBe( 57 );
-	} );
-} );
+	it('Selector will gather rows uniquely', function() {
+		expect(table.rows(['tr', 'tr']).count()).toBe(57);
+	});
+});
