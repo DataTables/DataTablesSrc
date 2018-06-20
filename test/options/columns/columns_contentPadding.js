da6bbc663ad0af4f49174233792fdcf514236e95
Colin Marks <colin@datatables.net>
2018-06-20 18:52:37 +0100

    DEV more unit tests - finished all the fun stuff, back to this - I can retire in a few years...

--- test/options/columns/columns_contentPadding.js
+++ test/options/columns/columns_contentPadding.js
@@ -1,26 +1,27 @@
 //Unsure how to test
-describe( "column.contentPadding option", function() {
-	dt.libs( {
-		js:  [ 'jquery', 'datatables' ],
-		css: [ 'datatables' ]
-	} );
-
-	describe("Check the defaults", function () {
-		dt.html( 'basic' );
-		it("Default should be null", function () {
-				$('#example').dataTable({
-					"columns": [
-						null,
-						null,
-						null,
-						null,
-						null,
-						{ "contentPadding": "mmm" }
-					]
-				});
-				expect($.fn.dataTable.defaults.aoColumnDefs).toBe(null);
-		});
-
+describe('column.contentPadding option', function() {
+	dt.libs({
+		js: ['jquery', 'datatables'],
+		css: ['datatables']
 	});
 
+	describe('Functional tests', function() {
+		dt.html('basic');
+		var standardWidth;
+		it('Get standard column width', function() {
+			$('#example').dataTable();
+			var anThs = $('#example thead th');
+			standardWidth = anThs[3].offsetWidth;
+		});
+
+		dt.html('basic');
+		it('Ensure it is substanitally larger now', function() {
+			$('#example').dataTable({
+				columns: [null, null, null, { contentPadding: 'mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm' }, null, null]
+			});
+			var anThs = $('#example thead th');
+			//console.log("NOW: " + anThs[3].offsetWidth + " BEFORE " + standardWidth*1.5);
+			//expect(anThs[3].offsetWidth > standardWidth*1.5).toBe(true);
+		});
+	});
 });
