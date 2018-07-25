96352532155dd8677e0112bc624a72c834c8a35d
Colin Marks <colin@datatables.net>
2018-07-25 09:25:41 +0100

    DEV a few more tests

--- test/api/cells/cell().data().js
+++ test/api/cells/cell().data().js
@@ -1,26 +1,94 @@
 // todo tests
-// - Confirm it exists and is a function
-// - Getter:
-//   - Ensure that the data from the cell is returned
-//   - Use a renderer for a column, make sure that it is the data and not the rendered value that is returned
-// - Setter:
-//   - Returns an API instance
-//   - Data in the DOM is updated for the target cell
-//   - Filtering works on the new value
-//   - Sorting works on the new value
+
 //   - Number of rows in the table matches that before the update (i.e. doesn't add or remove a row by mistake)
 //   - Set a value for a column with a renderer - make sure the renderer is executed and the DOM updated with the rendered value
-describe( "cells- cell().data()", function() {
-	dt.libs( {
-		js:  [ 'jquery', 'datatables' ],
-		css: [ 'datatables' ]
-	} );
-
-	describe("Check the defaults", function () {
-		dt.html( 'basic' );
-		it("Default should be null", function () {
-				});
-
+describe('cells- cell().data()', function() {
+	dt.libs({
+		js: ['jquery', 'datatables'],
+		css: ['datatables']
 	});
 
+	describe('Check the defaults', function() {
+		dt.html('basic');
+		let table;
+		it('Exists and is a function', function() {
+			table = $('#example').DataTable();
+			expect(typeof table.cell().data).toBe('function');
+		});
+		it('Getter returns an array', function() {
+			expect(typeof table.cell().data()).toBe('string');
+		});
+		it('Setter returns an API instance', function() {
+			expect(table.cell().data('Fred') instanceof $.fn.dataTable.Api).toBe(true);
+		});
+	});
+
+	describe('Functional tests - Getter', function() {
+		dt.html('basic');
+		it('Returns expected data', function() {
+			let table = $('#example').DataTable();
+			expect(table.cell(2, 0).data()).toBe('Ashton Cox');
+		});
+
+		dt.html('basic');
+		it('Returns original data, not rendered', function() {
+			table = $('#example').DataTable({
+				columnDefs: [
+					{
+						targets: 0,
+						render: function(data) {
+							return data.toUpperCase();
+						}
+					}
+				]
+			});
+			expect($('#example tbody tr:eq(2) td:eq(0)').text()).toBe('ASHTON COX');
+			expect(table.cell(2, 0).data()).toBe('Ashton Cox');
+		});
+	});
+
+	describe('Functional tests - Setter', function() {
+		dt.html('basic');
+		let table;
+		it('Updates before the draw', function() {
+			table = $('#example').DataTable();
+			table.cell(2, 0).data('Fred');
+			expect($('#example tbody tr:eq(2) td:eq(0)').text()).toBe('Fred');
+			expect(table.cell(2, 0).data()).toBe('Fred');
+		});
+		it('Draw causes a reordering', function() {
+			table.draw();
+			expect($('#example tbody tr:eq(2) td:eq(0)').text()).toBe('Bradley Greer');
+			expect(table.cell(2, 0).data()).toBe('Fred');
+		});
+		it('Same number of rows (not duplicated)', function() {
+			expect(table.rows().count()).toBe(57);
+		});
+		it('Ordering uses new value', function() {
+			table.page(1).draw(false);
+			expect($('#example tbody tr:eq(6) td:eq(0)').text()).toBe('Fred');
+		});
+		it('Filtering uses new value', function() {
+			table.search('Fred').draw();
+			expect($('#example tbody tr').length).toBe(1);
+			expect($('#example tbody tr:eq(0) td:eq(0)').text()).toBe('Fred');
+		});
+
+		dt.html('basic');
+		it('Column renderer is still called', function() {
+			table = $('#example').DataTable({
+				columnDefs: [
+					{
+						targets: 0,
+						render: function(data) {
+							return data.toUpperCase();
+						}
+					}
+				]
+			});
+			table.cell(2, 0).data('Fred');
+			expect($('#example tbody tr:eq(2) td:eq(0)').text()).toBe('FRED');
+			expect(table.cell(2, 0).data()).toBe('Fred');
+		});
+	});
 });
