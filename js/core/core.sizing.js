a4c27d42292d020e86e3c9d154123e27b238bdc1
Allan Jardine <allan@stargazer>
2018-08-13 21:20:48 +0100

    Update: Making the auto column calculations more agressive. The worst case table is now computed rather than using information from the DOM, as a simplification.
    - Min size 83234

--- js/core/core.sizing.js
+++ js/core/core.sizing.js
@@ -43,154 +43,134 @@ function _fnCalculateColumnWidths ( oSettings )
 		}
 	}
 
-	/* If the number of columns in the DOM equals the number that we have to
-	 * process in DataTables, then we can use the offsets that are created by
-	 * the web- browser. No custom sizes can be set in order for this to happen,
-	 * nor scrolling used
-	 */
-	if ( ie67 || ! userInputs && ! scrollX && ! scrollY &&
-	     columnCount == _fnVisbleColumns( oSettings ) &&
-	     columnCount == headerCells.length
-	) {
-		for ( i=0 ; i<columnCount ; i++ ) {
-			var colIdx = _fnVisibleToColumnIndex( oSettings, i );
+	// Construct a single row, worst case, table with the widest
+	// node in the data, assign any user defined widths, then insert it into
+	// the DOM and allow the browser to do all the hard work of calculating
+	// table widths
+	var tmpTable = $(table).clone() // don't use cloneNode - IE8 will remove events on the main table
+		.css( 'visibility', 'hidden' )
+		.removeAttr( 'id' );
 
-			if ( colIdx !== null ) {
-				columns[ colIdx ].sWidth = _fnStringToCss( headerCells.eq(i).width() );
+	// Clean up the table body
+	tmpTable.find('tbody tr').remove();
+	var tr = $('<tr/>').appendTo( tmpTable.find('tbody') );
+
+	// Clone the table header and footer - we can't use the header / footer
+	// from the cloned table, since if scrolling is active, the table's
+	// real header and footer are contained in different table tags
+	tmpTable.find('thead, tfoot').remove();
+	tmpTable
+		.append( $(oSettings.nTHead).clone() )
+		.append( $(oSettings.nTFoot).clone() );
+
+	// Remove any assigned widths from the footer (from scrolling)
+	tmpTable.find('tfoot th, tfoot td').css('width', '');
+
+	// Apply custom sizing to the cloned header
+	tmpTable.find('thead th, thead td').each( function () {
+		// Get the `width` from the header layout
+		var width = _fnColumnsSumWidth( oSettings, this, true, false );
+
+		if ( width ) {
+			this.style.width = width;
+
+			// For scrollX we need to force the column width otherwise the
+			// browser will collapse it. If this width is smaller than the
+			// width the column requires, then it will have no effect
+			if ( scrollX ) {
+				$( this ).append( $('<div/>').css( {
+					width: column.sWidthOrig,
+					margin: 0,
+					padding: 0,
+					border: 0,
+					height: 1
+				} ) );
 			}
+		}
+	} );
+
+	// Find the widest cell for each column and put it into the table
+	if ( oSettings.aoData.length ) {
+		for ( i=0 ; i<visibleColumns.length ; i++ ) {
+			columnIdx = visibleColumns[i];
+			column = columns[ columnIdx ];
+
+			$( _fnGetWidestNode( oSettings, columnIdx ) )
+				.clone( false )
+				.append( column.sContentPadding )
+				.appendTo( tr );
 		}
 	}
-	else
-	{
-		// Otherwise construct a single row, worst case, table with the widest
-		// node in the data, assign any user defined widths, then insert it into
-		// the DOM and allow the browser to do all the hard work of calculating
-		// table widths
-		var tmpTable = $(table).clone() // don't use cloneNode - IE8 will remove events on the main table
-			.css( 'visibility', 'hidden' )
-			.removeAttr( 'id' );
 
-		// Clean up the table body
-		tmpTable.find('tbody tr').remove();
-		var tr = $('<tr/>').appendTo( tmpTable.find('tbody') );
+	// Tidy the temporary table - remove name attributes so there aren't
+	// duplicated in the dom (radio elements for example)
+	$('[name]', tmpTable).removeAttr('name');
 
-		// Clone the table header and footer - we can't use the header / footer
-		// from the cloned table, since if scrolling is active, the table's
-		// real header and footer are contained in different table tags
-		tmpTable.find('thead, tfoot').remove();
-		tmpTable
-			.append( $(oSettings.nTHead).clone() )
-			.append( $(oSettings.nTFoot).clone() );
+	// Table has been built, attach to the document so we can work with it.
+	// A holding element is used, positioned at the top of the container
+	// with minimal height, so it has no effect on if the container scrolls
+	// or not. Otherwise it might trigger scrolling when it actually isn't
+	// needed
+	var holder = $('<div/>').css( scrollX || scrollY ?
+			{
+				position: 'absolute',
+				top: 0,
+				left: 0,
+				height: 1,
+				right: 0,
+				overflow: 'hidden'
+			} :
+			{}
+		)
+		.append( tmpTable )
+		.appendTo( tableContainer );
 
-		// Remove any assigned widths from the footer (from scrolling)
-		tmpTable.find('tfoot th, tfoot td').css('width', '');
+	// When scrolling (X or Y) we want to set the width of the table as 
+	// appropriate. However, when not scrolling leave the table width as it
+	// is. This results in slightly different, but I think correct behaviour
+	if ( scrollX && scrollXInner ) {
+		tmpTable.width( scrollXInner );
+	}
+	else if ( scrollX ) {
+		tmpTable.css( 'width', 'auto' );
+		tmpTable.removeAttr('width');
 
-		// Apply custom sizing to the cloned header
-		tmpTable.find('thead th, thead td').each( function () {
-			// Get the `width` from the header layout
-			var width = _fnColumnsSumWidth( oSettings, this, true, false );
-
-			if ( width ) {
-				this.style.width = width;
-
-				// For scrollX we need to force the column width otherwise the
-				// browser will collapse it. If this width is smaller than the
-				// width the column requires, then it will have no effect
-				if ( scrollX ) {
-					$( this ).append( $('<div/>').css( {
-						width: column.sWidthOrig,
-						margin: 0,
-						padding: 0,
-						border: 0,
-						height: 1
-					} ) );
-				}
-			}
-		} );
-
-		// Find the widest cell for each column and put it into the table
-		if ( oSettings.aoData.length ) {
-			for ( i=0 ; i<visibleColumns.length ; i++ ) {
-				columnIdx = visibleColumns[i];
-				column = columns[ columnIdx ];
-
-				$( _fnGetWidestNode( oSettings, columnIdx ) )
-					.clone( false )
-					.append( column.sContentPadding )
-					.appendTo( tr );
-			}
-		}
-
-		// Tidy the temporary table - remove name attributes so there aren't
-		// duplicated in the dom (radio elements for example)
-		$('[name]', tmpTable).removeAttr('name');
-
-		// Table has been built, attach to the document so we can work with it.
-		// A holding element is used, positioned at the top of the container
-		// with minimal height, so it has no effect on if the container scrolls
-		// or not. Otherwise it might trigger scrolling when it actually isn't
-		// needed
-		var holder = $('<div/>').css( scrollX || scrollY ?
-				{
-					position: 'absolute',
-					top: 0,
-					left: 0,
-					height: 1,
-					right: 0,
-					overflow: 'hidden'
-				} :
-				{}
-			)
-			.append( tmpTable )
-			.appendTo( tableContainer );
-
-		// When scrolling (X or Y) we want to set the width of the table as 
-		// appropriate. However, when not scrolling leave the table width as it
-		// is. This results in slightly different, but I think correct behaviour
-		if ( scrollX && scrollXInner ) {
-			tmpTable.width( scrollXInner );
-		}
-		else if ( scrollX ) {
-			tmpTable.css( 'width', 'auto' );
-			tmpTable.removeAttr('width');
-
-			// If there is no width attribute or style, then allow the table to
-			// collapse
-			if ( tmpTable.width() < tableContainer.clientWidth && tableWidthAttr ) {
-				tmpTable.width( tableContainer.clientWidth );
-			}
-		}
-		else if ( scrollY ) {
+		// If there is no width attribute or style, then allow the table to
+		// collapse
+		if ( tmpTable.width() < tableContainer.clientWidth && tableWidthAttr ) {
 			tmpTable.width( tableContainer.clientWidth );
 		}
-		else if ( tableWidthAttr ) {
-			tmpTable.width( tableWidthAttr );
-		}
-
-		// Get the width of each column in the constructed table
-		var total = 0;
-		var bodyCells = tmpTable.find('tbody tr').eq(0).children();
-
-		for ( i=0 ; i<visibleColumns.length ; i++ ) {
-			// Use getBounding... where possible (not IE8-) because it can give
-			// sub-pixel accuracy, which we then want to round up!
-			var bounding = browser.bBounding ?
-				Math.ceil( bodyCells[i].getBoundingClientRect().width ) :
-				$(bodyCells[i]).outerWidth();
-
-			// Total is tracked to remove any sub-pixel errors as the outerWidth
-			// of the table might not equal the total given here (IE!).
-			total += bounding;
-
-			// Width for each column to use
-			columns[ visibleColumns[i] ].sWidth = _fnStringToCss( bounding );
-		}
-
-		table.style.width = _fnStringToCss( total );
-
-		// Finished with the table - ditch it
-		//holder.remove();
 	}
+	else if ( scrollY ) {
+		tmpTable.width( tableContainer.clientWidth );
+	}
+	else if ( tableWidthAttr ) {
+		tmpTable.width( tableWidthAttr );
+	}
+
+	// Get the width of each column in the constructed table
+	var total = 0;
+	var bodyCells = tmpTable.find('tbody tr').eq(0).children();
+
+	for ( i=0 ; i<visibleColumns.length ; i++ ) {
+		// Use getBounding... where possible (not IE8-) because it can give
+		// sub-pixel accuracy, which we then want to round up!
+		var bounding = browser.bBounding ?
+			Math.ceil( bodyCells[i].getBoundingClientRect().width ) :
+			$(bodyCells[i]).outerWidth();
+
+		// Total is tracked to remove any sub-pixel errors as the outerWidth
+		// of the table might not equal the total given here (IE!).
+		total += bounding;
+
+		// Width for each column to use
+		columns[ visibleColumns[i] ].sWidth = _fnStringToCss( bounding );
+	}
+
+	table.style.width = _fnStringToCss( total );
+
+	// Finished with the table - ditch it
+	holder.remove();
 
 	// If there is a width attr, we want to attach an event listener which
 	// allows the table sizing to automatically adjust when the window is
