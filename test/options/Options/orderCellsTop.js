e2513aa9e351a0cb9382d37318625ad2ae885a3e
Colin Marks <colin@datatables.net>
2018-08-15 11:58:00 +0100

    DEV more unit tests

--- test/options/Options/orderCellsTop.js
+++ test/options/Options/orderCellsTop.js
@@ -3,36 +3,34 @@ describe('orderCellsTop option', function() {
 		js: ['jquery', 'datatables'],
 		css: ['datatables']
 	});
-	//default only sorting on bottom, turned on top one, test with 3 rows and tests with rowspan
 	describe('Check the defaults', function() {
-		// dt.html( 'basic' );
+		dt.html('basic');
 		it('disabled by default- Using 1 row in header', function() {
-			//could you also add a check for which cell `column().header()` returns in the orderCellsTop option please?
-			$('#example').dataTable();
 			expect($.fn.dataTable.defaults.bSortCellsTop).toBe(false);
 		});
-		dt.html('basic');
 		it('Disabled by default- Using 2 rows in header', function() {
 			$('#example thead').append(
-				'<tr><th>Name</th><th>Position</th><th>Office</th><th>Age</th><th>Start date</th><th>Salary</th></tr>'
+				'<tr><th>Name1</th><th>Position1</th><th>Office1</th><th>Age1</th><th>Start date1</th><th>Salary1</th></tr>'
 			);
-			$('#example').dataTable();
+			let table = $('#example').DataTable();
 
-			expect($('#example thead tr:eq(1) th:eq(0)').hasClass('sorting_asc')).toBe(true);
 			expect($('#example thead tr:eq(0) th:eq(0)').hasClass('sorting_asc')).toBe(false);
+			expect($('#example thead tr:eq(1) th:eq(0)').hasClass('sorting_asc')).toBe(true);
+			expect($(table.column(0).header()).text()).toBe('Name1');
 		});
+
 		dt.html('basic');
 		it('Can be turned on- Using 2 rows in header', function() {
 			$('#example thead').append(
-				'<tr><th>Name</th><th>Position</th><th>Office</th><th>Age</th><th>Start date</th><th>Salary</th></tr>'
+				'<tr><th>Name1</th><th>Position1</th><th>Office1</th><th>Age1</th><th>Start date1</th><th>Salary1</th></tr>'
 			);
-
-			$('#example').dataTable({
+			let table = $('#example').DataTable({
 				orderCellsTop: true
 			});
 
 			expect($('#example thead tr:eq(0) th:eq(0)').hasClass('sorting_asc')).toBe(true);
 			expect($('#example thead tr:eq(1) th:eq(0)').hasClass('sorting_asc')).toBe(false);
+			expect($(table.column(0).header()).text()).toBe('Name');
 		});
 	});
 
@@ -40,61 +38,123 @@ describe('orderCellsTop option', function() {
 		dt.html('basic');
 		it('Disabled by default- Using 3 rows in header', function() {
 			$('#example thead').append(
-				'<tr><th>Name</th><th>Position</th><th>Office</th><th>Age</th><th>Start date</th><th>Salary</th></tr>'
+				'<tr><th>Name1</th><th>Position1</th><th>Office1</th><th>Age1</th><th>Start date1</th><th>Salary1</th></tr>'
 			);
 			$('#example thead').append(
-				'<tr><th>Name</th><th>Position</th><th>Office</th><th>Age</th><th>Start date</th><th>Salary</th></tr>'
+				'<tr><th>Name2</th><th>Position2</th><th>Office2</th><th>Age2</th><th>Start date2</th><th>Salary2</th></tr>'
 			);
-			$('#example').dataTable();
+			let table = $('#example').DataTable();
 
-			expect($('#example thead tr:eq(2) th:eq(0)').hasClass('sorting_asc')).toBe(true);
-			expect($('#example thead tr:eq(1) th:eq(0)').hasClass('sorting_asc')).toBe(false);
 			expect($('#example thead tr:eq(0) th:eq(0)').hasClass('sorting_asc')).toBe(false);
+			expect($('#example thead tr:eq(1) th:eq(0)').hasClass('sorting_asc')).toBe(false);
+			expect($('#example thead tr:eq(2) th:eq(0)').hasClass('sorting_asc')).toBe(true);
+			expect($(table.column(0).header()).text()).toBe('Name2');
 		});
+
 		dt.html('basic');
 		it('Can be turned on- Using 3 rows in header', function() {
 			$('#example thead').append(
-				'<tr><th>Name</th><th>Position</th><th>Office</th><th>Age</th><th>Start date</th><th>Salary</th></tr>'
+				'<tr><th>Name1</th><th>Position1</th><th>Office1</th><th>Age1</th><th>Start date1</th><th>Salary1</th></tr>'
 			);
 			$('#example thead').append(
-				'<tr><th>Name</th><th>Position</th><th>Office</th><th>Age</th><th>Start date</th><th>Salary</th></tr>'
+				'<tr><th>Name2</th><th>Position2</th><th>Office2</th><th>Age2</th><th>Start date2</th><th>Salary2</th></tr>'
 			);
-			$('#example').dataTable({
+			let table = $('#example').DataTable({
 				orderCellsTop: true
 			});
 
 			expect($('#example thead tr:eq(0) th:eq(0)').hasClass('sorting_asc')).toBe(true);
 			expect($('#example thead tr:eq(1) th:eq(0)').hasClass('sorting_asc')).toBe(false);
 			expect($('#example thead tr:eq(2) th:eq(0)').hasClass('sorting_asc')).toBe(false);
+			expect($(table.column(0).header()).text()).toBe('Name');
 		});
 	});
 
-	describe('Test when using ColSpan- 2 rows', function() {
+	describe('Ignored when using ColSpan- 2 rows', function() {
 		dt.html('basic');
 		it('Disabled by default when using ColSpan- 2 rows', function() {
 			$('#example thead').prepend('<tr><th colspan="3">HR Information</th><th colspan="3">Contact</th></tr>');
-			$('#example thead tr:eq(1)').remove();
-			$('#example thead').append(
-				'<tr><th>Name</th><th>Position</th><th>Office</th><th>Age</th><th>Start date</th><th>Salary</th></tr>'
-			);
-			$('#example').dataTable();
+			let table = $('#example').DataTable();
 
 			expect($('#example thead tr:eq(0) th:eq(0)').hasClass('sorting_asc')).toBe(false);
 			expect($('#example thead tr:eq(1) th:eq(0)').hasClass('sorting_asc')).toBe(true);
+			expect($(table.column(0).header()).text()).toBe('Name');
 		});
+
 		dt.html('basic');
 		it('Enabled when using ColSpan- 2 rows', function() {
 			$('#example thead').prepend('<tr><th colspan="3">HR Information</th><th colspan="3">Contact</th></tr>');
-			$('#example thead tr:eq(1)').remove();
-			$('#example thead').append(
-				'<tr><th>Name</th><th>Position</th><th>Office</th><th>Age</th><th>Start date</th><th>Salary</th></tr>'
-			);
-			$('#example').dataTable({
+			let table = $('#example').DataTable({
 				orderCellsTop: true
 			});
 
 			expect($('#example thead tr:eq(0) th:eq(0)').hasClass('sorting_asc')).toBe(false);
 			expect($('#example thead tr:eq(1) th:eq(0)').hasClass('sorting_asc')).toBe(true);
+			expect($(table.column(0).header()).text()).toBe('Name');
+		});
+	});
+
+	describe('When using RowSpan', function() {
+		dt.html('basic');
+		it('Disabled by default when using RowSpan (first column, 2 rows)', function() {
+			$('#example thead').prepend(
+				'<tr><th rowspan="2">Name</th><th>Position</th><th>Office</th><th>Age</th><th>Start date</th><th>Salary</th></tr>'
+			);
+			$('#example thead tr:eq(1)').remove();
+			$('#example thead').append(
+				'<tr><th>Position1</th><th>Office1</th><th>Age1</th><th>Start date1</th><th>Salary1</th></tr>'
+			);
+			let table = $('#example').DataTable();
+
+			expect($('#example thead tr:eq(0) th').hasClass('sorting_asc')).toBe(true);
+			expect($(table.column(0).header()).text()).toBe('Name');
+		});
+
+		dt.html('basic');
+		it('Enabled when using RowSpan (first column, 2 rows)', function() {
+			$('#example thead').prepend(
+				'<tr><th rowspan="2">Name</th><th>Position</th><th>Office</th><th>Age</th><th>Start date</th><th>Salary</th></tr>'
+			);
+			$('#example thead tr:eq(1)').remove();
+			$('#example thead').append(
+				'<tr><th>Position1</th><th>Office1</th><th>Age1</th><th>Start date1</th><th>Salary1</th></tr>'
+			);
+			let table = $('#example').DataTable({
+				orderCellsTop: true
+			});
+
+			expect($('#example thead tr:eq(0) th').hasClass('sorting_asc')).toBe(true);
+			expect($(table.column(0).header()).text()).toBe('Name');
+		});
+
+		dt.html('basic');
+		it('Disabled by default when using RowSpan (second column, 2 rows)', function() {
+			$('#example thead').prepend(
+				'<tr><th>Name</th><th rowspan="2">Position</th><th>Office</th><th>Age</th><th>Start date</th><th>Salary</th></tr>'
+			);
+			$('#example thead tr:eq(1)').remove();
+			$('#example thead').append('<tr><th>Name1</th><th>Office1</th><th>Age1</th><th>Start date1</th><th>Salary1</th></tr>');
+			let table = $('#example').DataTable();
+
+			expect($('#example thead tr:eq(0) th:eq(0)').hasClass('sorting_asc')).toBe(false);
+			expect($('#example thead tr:eq(1) th:eq(0)').hasClass('sorting_asc')).toBe(true);
+			expect($(table.column(0).header()).text()).toBe('Name1');
+		});
+
+		dt.html('basic');
+		it('Enabled when using RowSpan (second column, 2 rows)', function() {
+			$('#example thead').prepend(
+				'<tr><th>Name</th><th rowspan="2">Position</th><th>Office</th><th>Age</th><th>Start date</th><th>Salary</th></tr>'
+			);
+			$('#example thead tr:eq(1)').remove();
+			$('#example thead').append('<tr><th>Name1</th><th>Office1</th><th>Age1</th><th>Start date1</th><th>Salary1</th></tr>');
+			let table = $('#example').DataTable({
+				orderCellsTop: true
+			});
+
+			expect($('#example thead tr:eq(0) th:eq(0)').hasClass('sorting_asc')).toBe(true);
+			expect($('#example thead tr:eq(1) th:eq(0)').hasClass('sorting_asc')).toBe(false);
+			expect($(table.column(0).header()).text()).toBe('Name');
 		});
 	});
 });
--- test/options/Options/pagingType.js
+++ test/options/Options/pagingType.js
@@ -1,64 +1,243 @@
-describe( "pageType Option", function() {
-	dt.libs( {
-		js:  [ 'jquery', 'datatables' ],
-		css: [ 'datatables' ]
-	} );
-
-	describe("Check the defaults", function () {
-		dt.html( 'basic' );
-		it("Check simple numbers paging is the default", function () {
-			$('#example').dataTable( );
-			expect($('#example').DataTable.settings[0].sPaginationType == 'simple_numbers').toBe(true);
-		});
-		it("Check class is applied", function () {
-			expect($('#example_paginate').hasClass('paging_simple_numbers')).toBe(true);
-		});
-		it("8 A elements are in the wrapper for test data", function () {
-			expect($('#example_paginate a').length == 8).toBe(true);
-		});
-		it("We have the previous button", function () {
-			expect(document.getElementById('example_previous')).toBeTruthy();
-		});
-		it("We have the next button", function () {
-			expect(document.getElementById('example_next')).toBeTruthy();
-		});
-		it("Previous button is disabled", function () {
-			expect($('#example_previous').hasClass('disabled')).toBe(true);
-		});
-		it("Next button is enabled", function () {
-			expect($('#example_next').hasClass('disabled')).toBe(false);
-		});
-		//Tests for paging are done in dt-zero_config.test.js
+describe('pageType Option', function() {
+	dt.libs({
+		js: ['jquery', 'datatables'],
+		css: ['datatables']
 	});
-	describe("Full buttons paging", function () {
-		dt.html( 'basic' );
-		it("Can enabled full numbers paging", function () {
-			$('#example').dataTable( {
-				"pagingType": "full_numbers"
+
+	function checkPaging(count, typeInternal, type, pages, pageCount, prevNext, firstLast) {
+		expect($('.dataTables_paginate a').length).toBe(count);
+		expect($('#example').DataTable.settings[0].sPaginationType).toBe(typeInternal);
+
+		if (pages) {
+			// this is the sum of the numbers ('a') and the ellipses ('span')
+			expect($('.dataTables_paginate').children('span').length).toBe(1);
+			let a = $('.dataTables_paginate span a').length;
+			let e = $('.dataTables_paginate')
+				.children('span')
+				.children('span').length;
+
+			expect(a + e).toBe(pageCount);
+		}
+
+		if (prevNext) {
+			expect($('.dataTables_paginate .previous').length).toBe(1);
+			expect($('.dataTables_paginate .next').length).toBe(1);
+		}
+
+		if (firstLast) {
+			expect($('.dataTables_paginate .first').length).toBe(1);
+			expect($('.dataTables_paginate .last').length).toBe(1);
+		}
+
+		return true;
+	}
+
+	describe('Check the various paging types in default table', function() {
+		dt.html('basic');
+		it('Default', function() {
+			$('#example').dataTable();
+			expect(checkPaging(8, 'simple_numbers', 'paging_simple_numbers', true, 6, true, false)).toBe(true);
+		});
+
+		dt.html('basic');
+		it('Numbers', function() {
+			$('#example').dataTable({
+				pagingType: 'numbers'
 			});
-			expect($('#example').DataTable.settings[0].sPaginationType == "full_numbers").toBe(true);
+			expect(checkPaging(6, 'numbers', 'paging_numbers', true, 6, false, false)).toBe(true);
 		});
-		it("Check full numbers class is applied", function () {
-			expect($('#example_paginate').hasClass("paging_full_numbers")).toBe(true);
+
+		dt.html('basic');
+		it('Simple', function() {
+			$('#example').dataTable({
+				pagingType: 'simple'
+			});
+			expect(checkPaging(2, 'simple', 'paging_simple', false, undefined, true, false)).toBe(true);
 		});
-		it("Jump to last page", function () {
-			$('#example_last').click();
-			expect(document.getElementById('example_info').innerHTML == "Showing 51 to 57 of 57 entries").toBe(true);
+
+		dt.html('basic');
+		it('Simple Numbers', function() {
+			$('#example').dataTable({
+				pagingType: 'simple_numbers'
+			});
+			expect(checkPaging(8, 'simple_numbers', 'paging_simple_numbers', true, 6, true, false)).toBe(true);
 		});
-		it("Go to two pages previous", function () {
-			$('#example_previous').click();
-			$('#example_previous').click();
-			expect(document.getElementById('example_info').innerHTML == "Showing 31 to 40 of 57 entries").toBe(true);
+
+		dt.html('basic');
+		it('Full', function() {
+			$('#example').dataTable({
+				pagingType: 'full'
+			});
+			expect(checkPaging(4, 'full', 'paging_full', false, undefined, true, true)).toBe(true);
 		});
-		it("Next (second last) page", function () {
-			$('#example_next').click();
-			expect(document.getElementById('example_info').innerHTML == "Showing 41 to 50 of 57 entries").toBe(true);
+
+		dt.html('basic');
+		it('Full Numbers', function() {
+			$('#example').dataTable({
+				pagingType: 'full_numbers'
+			});
+			expect(checkPaging(10, 'full_numbers', 'paging_full_numbers', true, 6, true, true)).toBe(true);
 		});
-		it("Jump to first page", function () {
-			$('#example_first').click();
-			expect(document.getElementById('example_info').innerHTML == "Showing 1 to 10 of 57 entries").toBe(true);
+
+		dt.html('basic');
+		it('First Last Numbers', function() {
+			$('#example').dataTable({
+				pagingType: 'first_last_numbers'
+			});
+			expect(checkPaging(8, 'first_last_numbers', 'paging_first_last_numbers', true, 6, false, true)).toBe(true);
 		});
 	});
 
+	describe('Check the various paging types when long pageLength', function() {
+		dt.html('basic');
+		it('Default', function() {
+			$('#example').dataTable({
+				pageLength: 60
+			});
+			expect(checkPaging(3, 'simple_numbers', 'paging_simple_numbers', true, 1, true, false)).toBe(true);
+		});
 
+		dt.html('basic');
+		it('Numbers', function() {
+			$('#example').dataTable({
+				pagingType: 'numbers',
+				pageLength: 60
+			});
+			expect(checkPaging(1, 'numbers', 'paging_numbers', true, 1, false, false)).toBe(true);
+		});
+
+		dt.html('basic');
+		it('Simple', function() {
+			$('#example').dataTable({
+				pagingType: 'simple',
+				pageLength: 60
+			});
+			expect(checkPaging(2, 'simple', 'paging_simple', false, undefined, true, false)).toBe(true);
+		});
+
+		dt.html('basic');
+		it('Simple Numbers', function() {
+			$('#example').dataTable({
+				pagingType: 'simple_numbers',
+				pageLength: 60
+			});
+			expect(checkPaging(3, 'simple_numbers', 'paging_simple_numbers', true, 1, true, false)).toBe(true);
+		});
+
+		dt.html('basic');
+		it('Full', function() {
+			$('#example').dataTable({
+				pagingType: 'full',
+				pageLength: 60
+			});
+			expect(checkPaging(4, 'full', 'paging_full', false, undefined, true, true)).toBe(true);
+		});
+
+		dt.html('basic');
+		it('Full Numbers', function() {
+			$('#example').dataTable({
+				pagingType: 'full_numbers',
+				pageLength: 60
+			});
+			expect(checkPaging(5, 'full_numbers', 'paging_full_numbers', true, 1, true, true)).toBe(true);
+		});
+
+		dt.html('basic');
+		it('First Last Numbers', function() {
+			$('#example').dataTable({
+				pagingType: 'first_last_numbers',
+				pageLength: 60
+			});
+			expect(checkPaging(3, 'first_last_numbers', 'paging_first_last_numbers', true, 1, false, true)).toBe(true);
+		});
+	});
+
+	describe('Check the various paging types when short pageLength', function() {
+		dt.html('basic');
+		it('Default', function() {
+			$('#example').dataTable({
+				pageLength: 2
+			});
+			expect(checkPaging(8, 'simple_numbers', 'paging_simple_numbers', true, 7, true, false)).toBe(true);
+		});
+
+		dt.html('basic');
+		it('Numbers', function() {
+			$('#example').dataTable({
+				pagingType: 'numbers',
+				pageLength: 2
+			});
+			expect(checkPaging(6, 'numbers', 'paging_numbers', true, 7, false, false)).toBe(true);
+		});
+
+		dt.html('basic');
+		it('Simple', function() {
+			$('#example').dataTable({
+				pagingType: 'simple',
+				pageLength: 2
+			});
+			expect(checkPaging(2, 'simple', 'paging_simple', false, undefined, true, false)).toBe(true);
+		});
+
+		dt.html('basic');
+		it('Simple Numbers', function() {
+			$('#example').dataTable({
+				pagingType: 'simple_numbers',
+				pageLength: 2
+			});
+			expect(checkPaging(8, 'simple_numbers', 'paging_simple_numbers', true, 7, true, false)).toBe(true);
+		});
+
+		dt.html('basic');
+		it('Full', function() {
+			$('#example').dataTable({
+				pagingType: 'full',
+				pageLength: 2
+			});
+			expect(checkPaging(4, 'full', 'paging_full', false, undefined, true, true)).toBe(true);
+		});
+
+		dt.html('basic');
+		it('Full Numbers', function() {
+			$('#example').dataTable({
+				pagingType: 'full_numbers',
+				pageLength: 2
+			});
+			expect(checkPaging(10, 'full_numbers', 'paging_full_numbers', true, 7, true, true)).toBe(true);
+		});
+
+		dt.html('basic');
+		it('First Last Numbers', function() {
+			$('#example').dataTable({
+				pagingType: 'first_last_numbers',
+				pageLength: 2
+			});
+			expect(checkPaging(8, 'first_last_numbers', 'paging_first_last_numbers', true, 7, false, true)).toBe(true);
+		});
+	});
+
+	describe('General odds and ends', function() {
+		dt.html('basic');
+		it('Short page length when mid-table', function() {
+			$('#example').dataTable({
+				pageLength: 2,
+				displayStart: 20
+			});
+			expect(checkPaging(7, 'simple_numbers', 'paging_simple_numbers', true, 7, true, false)).toBe(true);
+			expect(
+				$('.dataTables_paginate')
+					.children('span')
+					.children('span').length
+			).toBe(2);
+		});
+
+		dt.html('basic');
+		it('Paging disabled', function() {
+			$('#example').dataTable({
+				pageLength: 2,
+				paging: false
+			});
+			expect($('.dataTables_paginate').length).toBe(0);
+		});
+	});
 });
