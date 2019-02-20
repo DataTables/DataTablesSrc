3e57ff80ba6b1618ae0905034233b9496b01df00
Colin Marks <colin@datatables.net>
2019-02-20 10:17:09 +0000

    DEV yesterdays unit tests

--- test/events/preDraw.js
+++ test/events/preDraw.js
@@ -1,26 +1,33 @@
-describe('core - events - draw', function() {
+describe('core - events - preDraw', function() {
 	dt.libs({
 		js: ['jquery', 'datatables'],
 		css: ['datatables']
 	});
 
 	let table;
-	let count = 0;
 	let params;
 	let firstCell;
+	let count = 0;
+
+	function checkResults(cnt, name) {
+		expect(cnt).toBe(count);
+		expect(name).toBe(firstCell);
+		count = 0;
+		firstCell = '';
+	}
 
 	describe('Check the defaults', function() {
 		dt.html('basic');
 		it('Called after the draw', function() {
 			table = $('#example').DataTable();
-			table.on('draw.dt', function() {
+			table.on('preDraw.dt', function() {
 				params = arguments;
 				firstCell = $('tbody tr:eq(0) td:eq(0)').text();
 			});
 
 			table.page(1).draw(false);
 
-			expect(firstCell).toBe('Charde Marshall');
+			expect(firstCell).toBe('Airi Satou');
 			expect($('tbody tr:eq(0) td:eq(0)').text()).toBe('Charde Marshall');
 		});
 		it('Called with expected parameters', function() {
@@ -30,39 +37,32 @@ describe('core - events - draw', function() {
 		});
 	});
 
-	function checkResults(cnt, name) {
-		expect(cnt).toBe(count);
-		expect(name).toBe(firstCell);
-		count = 0;
-		firstCell = '';
-	}
-
 	describe('Functional tests', function() {
 		dt.html('basic');
 		it('Called on initial draw', function() {
 			table = $('#example')
-				.on('draw.dt', function() {
+				.on('preDraw.dt', function() {
 					count++;
 					firstCell = $('tbody tr:eq(0) td:eq(0)').text();
 				})
 				.DataTable();
-			checkResults(1, 'Airi Satou');
+			checkResults(1, 'Tiger Nixon');
 		});
 		it('Called when API changes page', function() {
 			table.page(2).draw(false);
-			checkResults(1, 'Gloria Little');
+			checkResults(1, 'Airi Satou');
 		});
 		it('Called when API changes page length', function() {
 			table.page.len(25).draw(false);
-			checkResults(1, 'Airi Satou');
+			checkResults(1, 'Gloria Little');
 		});
 		it('Called when API does a search', function() {
 			table.search('cox').draw(false);
-			checkResults(1, 'Ashton Cox');
+			checkResults(1, 'Airi Satou');
 		});
 		it('Called when API clears search', function() {
 			table.search('').draw(false);
-			checkResults(1, 'Airi Satou');
+			checkResults(1, 'Ashton Cox');
 		});
 		it('Called when API does an order', function() {
 			table.order([1, 'asc']).draw(false);
@@ -70,36 +70,36 @@ describe('core - events - draw', function() {
 		});
 		it('Called when user changes page', function() {
 			$('span a.paginate_button:eq(1)').click();
-			checkResults(1, 'Jena Gaines');
+			checkResults(1, 'Airi Satou');
 		});
 		it('Called when user changes page length', function() {
 			$('div.dataTables_length select')
-				.val(10)
+				.val(25)
 				.change();
-			checkResults(1, 'Jennifer Acosta');
+			checkResults(1, 'Jena Gaines');
 		});
 		it('Called when user searches', function() {
 			$('div.dataTables_filter label input')
 				.val('cox')
 				.keyup();
-			checkResults(1, 'Ashton Cox');
+			checkResults(1, 'Jena Gaines');
 		});
 		it('Called when user clears search', function() {
 			$('div.dataTables_filter label input')
 				.val('')
 				.keyup();
-			checkResults(1, 'Airi Satou');
+			checkResults(1, 'Ashton Cox');
 		});
 		it('Called when user changes order', function() {
 			$('thead tr th:eq(2)').click();
-			checkResults(1, 'Gavin Joyce');
+			checkResults(1, 'Airi Satou');
 		});
 
 		dt.html('empty');
 		it('Load ajax data into an empty table', function(done) {
 			count = 0;
 			table = $('#example')
-				.on('draw.dt', function() {
+				.on('preDraw.dt', function() {
 					count++;
 					firstCell = $('tbody tr:eq(0) td:eq(0)').text();
 				})
@@ -107,7 +107,7 @@ describe('core - events - draw', function() {
 					columns: dt.getTestColumns(),
 					ajax: '/base/test/data/data.txt',
 					initComplete: function(settings, json) {
-						checkResults(2, 'Airi Satou');
+						checkResults(2, 'Loading...');
 						done();
 					}
 				});
