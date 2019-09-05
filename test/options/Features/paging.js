7d558460a9e9cda4b0184d9e2668cd2e759f2683
Colin Marks <colin@datatables.net>
2019-09-05 09:35:50 +0000

    DEV additional unit tests for paging

--- test/options/Features/paging.js
+++ test/options/Features/paging.js
@@ -9,28 +9,61 @@ describe('Paging option', function() {
 		it('Enabled by default', function() {
 			expect($.fn.dataTable.defaults.bPaginate).toBe(true);
 		});
-
 		it('Check table is paged', function() {
 			table = $('#example').DataTable();
 			expect($('#example tbody tr').length).toBe(10);
 			expect(table.page.info().pages).toBe(6);
 		});
-
 		it('Paging control is in the DOM', function() {
 			table = $('#example').DataTable();
 			expect($('div.dataTables_paginate')[0]).toBeInDOM();
 		});
-
 		it('Length control is in the DOM', function() {
 			expect($('div.dataTables_length')[0]).toBeInDOM();
 		});
-
 		it('Correct place in DOM', function() {
 			expect(
 				$('div.dataTables_paginate')
 					.prev()
 					.attr('class')
 			).toBe('dataTables_info');
+		});
+		it('Disabled previous element has a tab index of -1', function() {
+			expect($('.paginate_button.previous').attr('tabindex')).toBe('-1');
+			expect($('.paginate_button.previous').hasClass('disabled')).toBe(true);
+
+			// DD-1098/DD-1099
+			// expect($('.paginate_button.next').attr('tabindex')).toBe('0');
+			// expect($('.paginate_button.next').hasClass('disabled')).toBe(false);
+		});
+		it('Both buttons active in middle of the table', function() {
+			table.page(3).draw(false);
+
+			expect($('.paginate_button.previous').attr('tabindex')).toBe('0');
+			expect($('.paginate_button.previous').hasClass('disabled')).toBe(false);
+
+			// DD-1098/DD-1099
+			// expect($('.paginate_button.next').attr('tabindex')).toBe('0');
+			// expect($('.paginate_button.next').hasClass('disabled')).toBe(false);
+		});
+		it('Disabled next element has a tab index of -1', function() {
+			table.page(5).draw(false);
+
+			expect($('.paginate_button.previous').attr('tabindex')).toBe('0');
+			expect($('.paginate_button.previous').hasClass('disabled')).toBe(false);
+
+			// DD-1098/DD-1099// expect($('.paginate_button.next').attr('tabindex')).toBe('-1');
+			// expect($('.paginate_button.next').hasClass('disabled')).toBe(true);
+		});
+		it('Both disabled after a search', function() {
+			table.search('Ashton Cox').draw();
+
+			expect($('.paginate_button.previous').attr('tabindex')).toBe('-1');
+			expect($('.paginate_button.previous').hasClass('disabled')).toBe(true);
+
+			// DD-1098/DD-1099
+			// expect($('.paginate_button.next').attr('tabindex')).toBe('-1');
+			// expect($('.paginate_button.next').hasClass('disabled')).toBe(true);
 		});
 	});
 
