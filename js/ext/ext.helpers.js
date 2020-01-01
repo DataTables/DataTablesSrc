6111f7aee8ea907bab730121b4be49cd3c70bd79
Allan Jardine <allan@datatables.net>
2020-01-01 14:56:13 +0000

    Dev: Allow for `Date` types in moment formatter

--- js/ext/ext.helpers.js
+++ js/ext/ext.helpers.js
@@ -71,7 +71,7 @@ DataTable.render = {
 
 			// Shortcut. If `from` and `to` are the same, we are using the renderer to
 			// format for ordering, not display - its already in the display format.
-			if ( from === to && type !== 'sort' && type !== 'type' ) {
+			if ( from === to && type !== 'sort' && type !== 'type' && ! (d instanceof Date) ) {
 				return d;
 			}
 
