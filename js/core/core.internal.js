76e1a927f8de455a708af8e61d002d08538855f4
taskinoz <tristan@taskinoz.com>
2020-07-08 11:13:22 +1000

    Added .trim() polyfill

--- js/core/core.internal.js
+++ js/core/core.internal.js
@@ -278,3 +278,11 @@ if (! Array.isArray) {
         return Object.prototype.toString.call(arg) === '[object Array]';
     };
 }
+
+// .trim() polyfill
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/trim
+if (!String.prototype.trim) {
+  String.prototype.trim = function () {
+    return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+  };
+}
