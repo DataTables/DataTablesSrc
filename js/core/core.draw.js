fcad629b8274b8762835fa14c08aacd5a4705e86
Alex Henrie <alexhenrie24@gmail.com>
2020-10-27 22:45:45 -0600

    Remove redundant check from _fnCreateTr

--- js/core/core.draw.js
+++ js/core/core.draw.js
@@ -47,7 +47,7 @@ function _fnCreateTr ( oSettings, iRow, nTrIn, anTds )
 			cells.push( nTd );
 
 			// Need to create the HTML if new, or if a rendering function is defined
-			if ( create || ((!nTrIn || oCol.mRender || oCol.mData !== i) &&
+			if ( create || ((oCol.mRender || oCol.mData !== i) &&
 				 (!$.isPlainObject(oCol.mData) || oCol.mData._ !== i+'.display')
 			)) {
 				nTd.innerHTML = _fnGetCellData( oSettings, iRow, i, 'display' );
