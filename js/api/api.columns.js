3dfd7b47b4a10f02d6a2022b15b77124111dc3bb
Allan Jardine <allan@datatables.net>
2020-11-26 14:55:22 +0000

    Dev - fix: Getting footer cells was failing

--- js/api/api.columns.js
+++ js/api/api.columns.js
@@ -207,7 +207,7 @@ _api_registerPlural( 'columns().header()', 'column().header()', function ( selec
 _api_registerPlural( 'columns().footer()', 'column().footer()', function ( selector, opts ) {
 	return this.iterator( 'column', function ( settings, column ) {
 		return settings.aoFooter.length ?
-			[0][column].cell :
+			settings.aoFooter[0][column].cell :
 			null;
 	}, 1 );
 } );
