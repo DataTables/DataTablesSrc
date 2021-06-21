89cb022c9ab601028af9259d3cf4cef6011b7da1
Allan Jardine <allan@datatables.net>
2021-06-21 11:25:01 +0000

    Dev: Moving on to 1.11 development

--- js/DataTables.js
+++ js/DataTables.js
@@ -1,11 +1,11 @@
-/*! DataTables 1.10.26-dev
+/*! DataTables 1.11.0-dev
  * ©2008-2021 SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     DataTables
  * @description Paginate, search and order HTML tables
- * @version     1.10.26-dev
+ * @version     1.11.0-dev
  * @file        jquery.dataTables.js
  * @author      SpryMedia Ltd
  * @contact     www.datatables.net
@@ -169,7 +169,7 @@
 	 *  @type string
 	 *  @default Version number
 	 */
-	DataTable.version = "1.10.26-dev";
+	DataTable.version = "1.11.0-dev";
 
 	/**
 	 * Private data store, containing all of the settings objects that are
