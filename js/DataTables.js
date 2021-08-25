82e29b70c11f82a33c180362fcb7680f6032a624
Allan Jardine <allan@datatables.net>
2021-08-25 13:01:52 +0000

    DataTables 1.11.0!

--- js/DataTables.js
+++ js/DataTables.js
@@ -1,11 +1,11 @@
-/*! DataTables 1.11.0-dev
+/*! DataTables 1.11.0
  * ©2008-2021 SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     DataTables
  * @description Paginate, search and order HTML tables
- * @version     1.11.0-dev
+ * @version     1.11.0
  * @file        jquery.dataTables.js
  * @author      SpryMedia Ltd
  * @contact     www.datatables.net
@@ -178,7 +178,7 @@
 	 *  @type string
 	 *  @default Version number
 	 */
-	DataTable.version = "1.11.0-dev";
+	DataTable.version = "1.11.0";
 
 	/**
 	 * Private data store, containing all of the settings objects that are
