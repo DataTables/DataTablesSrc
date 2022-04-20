ab09832bca0bfd726a88b3d5fdb88519d9e62924
Colin Marks <colin0117@yahoo.com>
2022-04-20 12:52:11 +0100

    TEST added tests for luxon and moment datetime

---
+++ test/options/columns/columns_render_luxon.js
@@ -0,0 +1,83 @@
+describe('columns.render option', function () {
+	dt.libs({
+		js: ['jquery', 'datatables', 'luxon'],
+		css: ['datatables']
+	});
+
+	let table;
+
+	function checkRecord(record, name, date) {
+		expect($('tbody tr:eq(' + record + ') td:eq(0)').text()).toBe(name);
+		expect($('tbody tr:eq(' + record + ') td:eq(4)').text()).toBe(date);
+	}
+
+	describe('Functional tests', function () {
+		dt.html('basic');
+		it('Standard data', function () {
+			table = $('#example').DataTable({
+				order: [4, 'asc']
+			});
+			checkRecord(0, 'Jackson Bradshaw', '2008/09/26');
+		});
+
+		dt.html('ISO8601');
+		it('Standard ISO8601 data', function () {
+			table = $('#example').DataTable({
+				order: [4, 'asc']
+			});
+			checkRecord(0, 'Jackson Bradshaw', '2008-09-26');
+		});
+
+		dt.html('ISO8601');
+		it('Auto-render', function () {
+			table = $('#example').DataTable({
+				order: [4, 'asc'],
+				columnDefs: [
+					{
+						targets: 4,
+						render: DataTable.render.date()
+					}
+				]
+			});
+			checkRecord(0, 'Jackson Bradshaw', '26/09/2008');
+		});
+
+		dt.html('ISO8601');
+		it('Date rendering', function () {
+			table = $('#example').DataTable({
+				order: [4, 'asc'],
+				columnDefs: [
+					{
+						targets: 4,
+						render: DataTable.render.datetime('d MMM yyyy')
+					}
+				]
+			});
+			checkRecord(0, 'Jackson Bradshaw', '26 Sep 2008');
+		});
+
+		dt.html('dates_non_std');
+		it('Date conversion', function () {
+			table = $('#example').DataTable({
+				order: [4, 'asc'],
+				columnDefs: [
+					{
+						targets: 4,
+						render: DataTable.render.datetime('d MMM yyyy', 'MMM d, yy', 'en'),
+					}
+				]
+			});
+			checkRecord(0, 'Jackson Bradshaw', 'Sep 26, 08');
+		});
+
+		dt.html('dates_non_std');
+		it('Global date rendering', function () {
+			DataTable.datetime('d MMM yyyy');
+
+			table = $('#example').DataTable({
+				order: [4, 'asc']
+			});
+			checkRecord(0, 'Jackson Bradshaw', '26 Sep 2008');
+		});
+	});
+});
