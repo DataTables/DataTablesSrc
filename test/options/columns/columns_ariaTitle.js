a5a2eac6a3e4fbd6e6337f4012137ccc961bb96f
Allan Jardine <allan@datatables.net>
2023-08-04 11:03:17 +0100

    Tests: Update `columns.ariaTitle` to account for the changes in ARIA ordering languge string

--- test/options/columns/columns_ariaTitle.js
+++ test/options/columns/columns_ariaTitle.js
@@ -23,35 +23,35 @@ describe('columns.ariaTitle option', function () {
 				]
 			});
 
-			expect($('thead th:eq(3)').attr('aria-label')).toBe('Office: activate to sort column ascending');
+			expect($('thead th:eq(3)').attr('aria-label')).toBe('Office: Activate to sort');
 			expect($('thead th:eq(3)').text()).toBe('Office');
 		});
 		it('Just ariaTitle', function () {
-			expect($('thead th:eq(0)').attr('aria-label')).toBe('test1: activate to sort column descending');
+			expect($('thead th:eq(0)').attr('aria-label')).toBe('test1: Activate to invert sorting');
 			expect($('thead th:eq(0)').text()).toBe('First name');
 		});
 		it('Just ariaTitle', function () {
-			expect($('thead th:eq(1)').attr('aria-label')).toBe('test2: activate to sort column ascending');
+			expect($('thead th:eq(1)').attr('aria-label')).toBe('test2: Activate to sort');
 			expect($('thead th:eq(1)').text()).toBe('test2');
 		});
 		it('Both ariaTitle and title', function () {
-			expect($('thead th:eq(2)').attr('aria-label')).toBe('test3o: activate to sort column ascending');
+			expect($('thead th:eq(2)').attr('aria-label')).toBe('test3o: Activate to sort');
 			expect($('thead th:eq(2)').text()).toBe('test3t');
 		});
 		it('Just aria-label', function () {
-			expect($('thead th:eq(4)').attr('aria-label')).toBe('test4: activate to sort column ascending');
+			expect($('thead th:eq(4)').attr('aria-label')).toBe('test4: Activate to sort');
 			expect($('thead th:eq(4)').text()).toBe('Age');
 		});
 		it('Both ariaTitle and aria-label', function () {
-			expect($('thead th:eq(5)').attr('aria-label')).toBe('test5o: activate to sort column ascending');
+			expect($('thead th:eq(5)').attr('aria-label')).toBe('test5o: Activate to sort');
 			expect($('thead th:eq(5)').text()).toBe('Start date');
 		});
 		it('Both aria-label and title', function () {
-			expect($('thead th:eq(6)').attr('aria-label')).toBe('test6a: activate to sort column ascending');
+			expect($('thead th:eq(6)').attr('aria-label')).toBe('test6a: Activate to sort');
 			expect($('thead th:eq(6)').text()).toBe('test6t');
 		});
 		it('ariaTitle, aria-label, and title', function () {
-			expect($('thead th:eq(7)').attr('aria-label')).toBe('test7o: activate to sort column ascending');
+			expect($('thead th:eq(7)').attr('aria-label')).toBe('test7o: Activate to sort');
 			expect($('thead th:eq(7)').text()).toBe('test7t');
 		});
 	});
