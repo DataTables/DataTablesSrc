7521d6f2bd98de2cde2e82dff82230c180eee722
Murilo Dagustin <87674220+murilo-plantae@users.noreply.github.com>
2023-11-16 12:08:58 -0400

    Fix: Don't check colspan of child rows for non-auto generated rows
    * Remove the function that overwrites the children td colspan when already has colspan
    
    * Revert "Remove the function that overwrites the children td colspan when already has colspan"
    
    This reverts commit cf6a21e438be0854a44acbbba60661be33f9d1c7.
    
    * Add verification that checks if children row has only one td before overwriting the existing colspan

--- js/api/api.row.details.js
+++ js/api/api.row.details.js
@@ -180,7 +180,12 @@ var __details_events = function ( settings )
 				row = data[i];
 
 				if ( row._details ) {
-					row._details.children('td[colspan]').attr('colspan', visible );
+					row._details.each(function () {
+						var el = $(this);
+						if (el.children('td').length == 1) {
+							el.children('td[colspan]').attr('colspan', visible);
+						}
+					});
 				}
 			}
 		} );
