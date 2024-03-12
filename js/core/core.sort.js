193bd5951d08d65cc50c25c01e3d6ced62121aaf
Allan Jardine <allan@datatables.net>
2024-03-12 16:00:28 +0000

    Fix: Processing indicator was being cancelled before a draw after a click to sort action

--- js/core/core.sort.js
+++ js/core/core.sort.js
@@ -58,8 +58,11 @@ function _fnSortAttachListener(settings, node, selector, column, callback) {
 				setTimeout( function () {
 					_fnSort( settings );
 					_fnSortDisplay( settings, settings.aiDisplay );
-					_fnReDraw( settings, false, false );
+
+					// Sort processing done - redraw has its own processing display
 					_fnProcessingDisplay( settings, false );
+
+					_fnReDraw( settings, false, false );
 
 					if (callback) {
 						callback();
