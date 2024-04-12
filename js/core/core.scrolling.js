257c6678cece593faed8ad8739b85a9b05163ed8
Allan Jardine <allan@datatables.net>
2024-04-12 14:49:36 +0000

    Fix: Responsive, when used with scrolling, could result in column misalignment
    This was because of Responsive removing `col` elements, which messes up
    DataTables' indexing, which had assumed that the `col` elements would be
    present for every column in the DOM.

--- js/core/core.scrolling.js
+++ js/core/core.scrolling.js
@@ -220,20 +220,27 @@ function _fnScrollDraw ( settings )
 	// uses a cell which has a longer string, but isn't the widest! For example 
 	// "Chief Executive Officer (CEO)" is the longest string in the demo, but
 	// "Systems Administrator" is actually the widest string since it doesn't collapse.
+	// Note the use of translating into a column index to get the `col` element. This
+	// is because of Responsive which might remove `col` elements, knocking the alignment
+	// of the indexes out.
 	if (settings.aiDisplay.length) {
 		// Get the column sizes from the first row in the table
-		var colSizes = table.find('tbody tr').eq(0).find('th, td').map(function () {
-			return $(this).outerWidth();
+		var colSizes = table.find('tbody tr').eq(0).find('th, td').map(function (vis) {
+			return {
+				idx: _fnVisibleToColumnIndex(settings, vis),
+				width: $(this).outerWidth()
+			}
 		});
 
 		// Check against what the colgroup > col is set to and correct if needed
-		$('col', settings.colgroup).each(function (i) {
-			var colWidth = this.style.width.replace('px', '');
+		for (var i=0 ; i<colSizes.length ; i++) {
+			var colEl = settings.aoColumns[ colSizes[i].idx ].colEl[0];
+			var colWidth = colEl.style.width.replace('px', '');
 
-			if (colWidth !== colSizes[i]) {
-				this.style.width = colSizes[i] + 'px';
+			if (colWidth !== colSizes[i].width) {
+				colEl.style.width = colSizes[i].width + 'px';
 			}
-		});
+		}
 	}
 
 	// 3. Copy the colgroup over to the header and footer
