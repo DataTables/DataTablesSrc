9f7bbc77a204392ce37883057264b4be2ebbfcb4
Allan Jardine <allan@datatables.net>
2025-01-23 17:11:28 +0000

    Fix: `-api ready()` would run with the wrong context if it had to wait for `-event init`.
    https://datatables.net/forums/discussion/80483/

--- js/api/api.core.js
+++ js/api/api.core.js
@@ -86,12 +86,14 @@ _api_register( 'ready()', function ( fn ) {
 	// Function to run either once the table becomes ready or
 	// immediately if it is already ready.
 	return this.tables().every(function () {
+		var api = this;
+
 		if (this.context[0]._bInitComplete) {
-			fn.call(this);
+			fn.call(api);
 		}
 		else {
 			this.on('init.dt.DT', function () {
-				fn.call(this);
+				fn.call(api);
 			});
 		}
 	} );
