8eb799488b2bd4973a34dd3a25b60880ecb531c4
Pavel Djundik <xPaw@users.noreply.github.com>
2025-03-09 19:12:34 +0200

    Typescript: Improve types of reduce() and reduceRight()
    Fixes #327

--- types/types.d.ts
+++ types/types.d.ts
@@ -1148,7 +1148,9 @@ export interface Api<T=any> {
      * @param initialValue Value to use as the first argument of the first call to the fn callback.
      * @returns Result from the final call to the fn callback function.
      */
-    reduce(fn: ((current: number, value: any, index: number, dt: Api<any>) => number), initialValue?: any): any;
+    reduce(callbackfn: (current: T, value: T, index: number, dt: Api<any>) => T): T;
+    reduce(callbackfn: (current: T, value: T, index: number, dt: Api<any>) => T, initialValue: T): T;
+    reduce<U>(callbackfn: (current: U, value: T, index: number, dt: Api<any>) => U, initialValue: U): U;
 
     /**
      * Apply a callback function against and accumulator and each element in the Api's result set (right-to-left).
@@ -1157,7 +1159,9 @@ export interface Api<T=any> {
      * @param initialValue Value to use as the first argument of the first call to the fn callback.
      * @returns Result from the final call to the fn callback function.
      */
-    reduceRight(fn: ((current: number, value: any, index: number, dt: Api<any>) => number), initialValue?: any): any;
+    reduceRight(callbackfn: (current: T, value: T, index: number, dt: Api<any>) => T): T;
+    reduceRight(callbackfn: (current: T, value: T, index: number, dt: Api<any>) => T, initialValue: T): T;
+    reduceRight<U>(callbackfn: (current: U, value: T, index: number, dt: Api<any>) => U, initialValue: U): U;
 
     /**
      * Reverse the result set of the API instance and return the original array.
