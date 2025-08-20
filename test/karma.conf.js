6d037fa022d0092ac023c9693dec53a4cf6eceda
Allan Jardine <allan@datatables.net>
2025-08-20 12:38:51 +0000

    Dev: Fix path for unit tests

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -127,7 +127,7 @@ module.exports = function(config) {
 			{ pattern: 'test/data/*.txt', included: false },
 			{ pattern: 'test/html/*.html', included: false },
 			{ pattern: 'extensions/*/test/html/*.html', included: false },
-			{ pattern: 'test/api/*.*.js', included: true },
+			{ pattern: 'test/api/*/*.js', included: true },
 			testFiles, extensionFiles
 		],
 
