d9a6bf32976e585c9a048af167b907c3fd14939a
Allan Jardine <allan@datatables.net>
2026-03-12 10:24:05 +0000

    Test: Provide a way for the extensions examples to be tested
    The `dt.sh` script on the build machine will call `npm run
    test-examples` with the required `DT_EXTENSION` environment var.

--- test-e2e/examples/link-setup.js
+++ test-e2e/examples/link-setup.js
@@ -8,20 +8,26 @@ const fs = require('fs');
 async function globalSetup(config) {
 	const browser = await chromium.launch();
 	const page = await browser.newPage();
+	const pageAddress = process.env.DT_EXTENSION
+		? `http://192.168.234.234/extensions/${process.env.DT_EXTENSION}/examples`
+		: 'http://192.168.234.234/examples/';
 
-	console.log('--- Starting Link Discovery ---');
-	await page.goto('http://192.168.234.234/examples/');
+	console.log(`--- Starting Link Discovery (${pageAddress}) ---`);
+
+	await page.goto(pageAddress);
 
 	const links = await page.evaluate(() => {
 		return (
 			Array.from(document.querySelectorAll('a'))
 				.map(a => a.href)
-				// Filter for links in the same directory and ignore common non-HTML files
+				// Filter for links in the same directory and ignore common
+				// non-HTML files
 				.filter(href => {
 					const isInternal = href.startsWith(
-						'http://192.168.234.234/examples/'
+						'http://192.168.234.234/'
 					);
 					const isFile = /\.(zip|pdf|docx|jpg|png|mp4)$/i.test(href);
+
 					return isInternal && !isFile;
 				})
 		);
