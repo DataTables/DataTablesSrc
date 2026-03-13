bddd58d641410a2caf30f436a052b1b1ec364f63
Allan Jardine <allan@datatables.net>
2026-03-13 10:29:27 +0000

    Fix: Accessabillity - remove the -tag tfoot element if it is empty
    https://datatables.net/forums/discussion/81708/
    Backported from https://github.com/DataTables/DataTablesSrc/commit/bab0644a047ff83509c29d362fd2bdfdb0f0c23a

--- js/core/core.init.js
+++ js/core/core.init.js
@@ -107,6 +107,12 @@ function _fnInitComplete ( settings )
 
 	var args = [settings, settings.json];
 
+	// If the footer element is empty after initialisation, then remove it
+	let tfoot = $(settings.tfoot);
+	if (tfoot.children().length === 0) {
+		tfoot.remove();
+	}
+
 	settings._bInitComplete = true;
 
 	// Table is fully set up and we have data, so calculate the
