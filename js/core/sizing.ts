36a4dac4f19a71ff83c53d9dc19664ad1cb4abf6
Allan Jardine <allan@datatables.net>
2026-03-31 15:45:56 +0000

    Fix: Multi-line html elements could still cause issues for column sizing

--- js/core/sizing.ts
+++ js/core/sizing.ts
@@ -334,9 +334,9 @@ function getWideStrings(settings: Context, colIdx: number) {
 			// Don't want script, dialog or template tags in the width
 			// calculations as they are hidden content
 			cellString = cellString
-				.replace(/<script.*?<\/script>/gi, ' ')
-				.replace(/<dialog.*?<\/dialog>/gi, ' ')
-				.replace(/<template.*?<\/template>/gi, ' ');
+				.replace(/<script[\s\S]*?<\/script>/gi, ' ')
+				.replace(/<dialog[\s\S]*?<\/dialog>/gi, ' ')
+				.replace(/<template[\s\S]*?<\/template>/gi, ' ');
 
 			var noHtml = util.string
 				.stripHtml(cellString, ' ')
