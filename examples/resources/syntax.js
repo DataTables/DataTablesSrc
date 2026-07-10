f7c34212a90417ae926b6959005b5b1f3978cf4c
Allan Jardine <allan@datatables.net>
2026-07-10 17:05:51 +0000

    Dev: Adding plain text option to the syntax highlighter

--- examples/resources/syntax.js
+++ examples/resources/syntax.js
@@ -8159,3 +8159,33 @@ hljs.addPlugin({
     }
   }
 });
+
+  (function(){
+    var hljsGrammar = (function () {
+  'use strict';
+
+  /*
+  Language: Plain text
+  Author: Egor Rogov (e.rogov@postgrespro.ru)
+  Description: Plain text without any highlighting.
+  Category: common
+  */
+
+  function plaintext(hljs) {
+    return {
+      name: 'Plain text',
+      aliases: [
+        'text',
+        'txt',
+        'plain'
+      ],
+      disableAutodetect: true
+    };
+  }
+
+  return plaintext;
+
+})();
+
+    hljs.registerLanguage('plaintext', hljsGrammar);
+  })();
