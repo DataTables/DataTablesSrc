1d903d8088e1d7b27b5c5704d2b9a9e7a6a63555
Allan Jardine <allan@datatables.net>
2026-09-21 13:40:27 +0000

    Fix: If the page had an element with the id `jQuery` and jQuery wasn't loaded, the element would be used as the jQuery library! This of course resulted in an error.
    This was happening on the DataTables compatibility page

--- js/util/external.ts
+++ js/util/external.ts
@@ -44,7 +44,17 @@ export default function (arg1: any, arg2?: any) {
 		switch (type) {
 			case 'lib':
 			case 'jq':
-				return __jquery !== undefined ? __jquery : getWin().jQuery || null;
+				if (__jquery) {
+					return __jquery;
+				}
+
+				let local = getWin().jQuery;
+
+				if (local && local.fn) {
+					return local;
+				}
+
+				return null;
 
 			case 'win':
 				return getWin();
